669c39f18727dddc7fbd40f40eafb5b113e4d769
Chris Warburton <chriswarbo@gmail.com>
2015-06-29 15:14:23 +0100

    It's simpler to use bash for testing

--- dump-package.sh
+++ dump-package.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Extract ASTs from the Cabal package in the current directory
+
 function packageName {
     (shopt -s nullglob
      for CBL in *.cabal
@@ -8,12 +10,23 @@ function packageName {
      done)
 }
 
-PKG=$(packageName)
-nix-shell -E "with import <nixpkgs> {}; ghcWithPlugin \"$PKG\"" \
-          --run "sh" <<'EOF'
+function extractAsts {
+    PKG=$(packageName)
+    nix-shell -E "with import <nixpkgs> {}; ghcWithPlugin \"$PKG\"" \
+              --run "sh" <<'EOF'
+# Get this shell's Haskell package database. The first line of `ghc-pkg list`
+# will tell us, except for a pesky ':' at the end of the line
 GHC_PKG=$(ghc-pkg list | head -n 1 | tr -d ':')
+
+# Tell GHC to use the right package database, to expose the AstPlugin package,
+# and to run the AstPlugin.Plugin during compilation
 PLUGIN="AstPlugin.Plugin"
 OPTIONS="-package-db=$GHC_PKG -package AstPlugin -fplugin=$PLUGIN"
-echo "$OPTIONS"
-cabal --ghc-options="$OPTIONS" build
+
+# Build the project, passing the above options through to GHC.
+# NOTE: AstPlugin writes to stderr, not stdout, so we redirect it (blame GHC!)
+cabal --ghc-options="$OPTIONS" build 2>&1 1>/dev/null
 EOF
+}
+
+extractAsts | grep "^FOUNDAST" | cut -d ' ' -f 2-
