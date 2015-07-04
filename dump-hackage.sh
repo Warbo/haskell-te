097db9422b5907d4fa208b4a2b69e3cf382a298e
Chris Warburton <chriswarbo@gmail.com>
2015-07-04 02:40:07 +0100

    Switch to sexpressions mostly

--- dump-hackage.sh
+++ dump-hackage.sh
@@ -3,9 +3,14 @@
 REL=$(dirname "$0")
 ABS=$(readlink -f "$REL")
 DIR=$(mktemp -d)
-cd "$DIR"
-nix-shell -p haskellPackages.cabal-install --run "cabal get $1" > /dev/null
+
+(cd "$DIR";
+ nix-shell -p haskellPackages.cabal-install --run "cabal get $1" > /dev/null)
+
 cd *
-"$ABS/dump-package.sh"
+for PROJECT in "$DIR"/*
+do
+    "$ABS/dump-package.sh" "$PROJECT"
+done
 cd
 rm -rf "$DIR"
