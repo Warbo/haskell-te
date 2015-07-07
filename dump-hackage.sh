7ade4551d926641a62b96650633803e6919aafab
Chris Warburton <chriswarbo@gmail.com>
2015-07-07 14:30:03 +0100

    Extract features into JSON

--- dump-hackage.sh
+++ dump-hackage.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+set -e
+set -x
 
 REL=$(dirname "$0")
 ABS=$(readlink -f "$REL")
 DIR=$(mktemp -d)
+PHASE="all"
+[[ "$#" -gt 1 ]] && PHASE="$2"
 
 (cd "$DIR";
  nix-shell -p haskellPackages.cabal-install --run "cabal get $1" > /dev/null)
 
-cd *
 for PROJECT in "$DIR"/*
 do
-    "$ABS/dump-package.sh" "$PROJECT"
+    ./dump-package.sh "$PROJECT" "$PHASE"
 done
-cd
+
 rm -rf "$DIR"
