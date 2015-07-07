994b396ef0c84f75e6214560b11113331d77abf0
Chris Warburton <chriswarbo@gmail.com>
2015-07-07 14:30:03 +0100

    Extract features into JSON

--- extractFeatures.sh
+++ extractFeatures.sh
@@ -1,10 +1,17 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -p treefeatures -p bash -i bash
+#! nix-shell -p treefeatures -p bash -p jq -i bash
 
-while read LINE
+set -e
+set -x
+
+while read -r LINE
 do
-    PREFIX=$(echo "$LINE" | cut -d '"' -f 1-2)
-    AST=$(echo "$LINE" | cut -d '"' -f 3- | grep -o "[^ ].*")
-    FEATURES=$(echo "$AST" | BITS=30 MODE=sexpr TreeFeatures)
-    echo "$PREFIX\" $FEATURES"
+    # Extract the "ast" value and pipe into TreeFeatures
+    FEATURES=$(echo "$LINE" | jq -r '.ast' | BITS=30 MODE=sexpr TreeFeatures)
+
+    # Read the features as a raw string, and collect into an array
+    FEATARR=$(echo "$FEATURES" | jq -R '.' | jq -s '.')
+
+    # Add the features to the object
+    echo "$LINE" | jq -c ". + {features: \"$FEATURES\"}"
 done
