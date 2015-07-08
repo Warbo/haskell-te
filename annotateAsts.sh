48715c14a93867b35aae4817036adf03f862bb5c
Chris Warburton <chriswarbo@gmail.com>
2015-07-08 15:07:42 +0100

    Add cluster to JSON

---
+++ annotateAsts.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -p jq -i bash
+
+set -e
+
+INPUT=$(cat)
+RAWASTS=$(echo  "$INPUT" | jq -c '.asts')
+RAWTYPES=$(echo "$INPUT" | jq -r '.result')
+
+echo "$RAWASTS" |
+    ./tagAsts.sh <(echo "$RAWTYPES" | ./getTypes.sh)   |
+    ./tagAsts.sh <(echo "$RAWTYPES" | ./getArities.sh) |
+    ./extractFeatures.sh |
+    ./cluster.sh
