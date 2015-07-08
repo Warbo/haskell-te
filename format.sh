8809739704df8485dcf57042f1480b840db41a4f
Chris Warburton <chriswarbo@gmail.com>
2015-07-08 15:25:25 +0100

    Format JSON

--- format.sh
+++ format.sh
@@ -1,35 +1,6 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell -p jq -i bash
 
-# Takes (sorted) input of the form:
-#
-# cluster1 foo
-# cluster1 bar
-# cluster2 baz
-#
-# Outputs each cluster as a separate line, eg.
-#
-# foo bar
-# baz
-
-LINES=$(cat)
-
-# The first line is special, as it has no preceding delimiter
-FIRST=$(echo "$LINES"   | head -n 1)
-CLUSTER=$(echo "$FIRST" | cut  -f 1)
-NAME=$(echo "$FIRST"    | cut  -f 2)
-printf "$NAME"
-
-# The rest of the lines are either " foo" or "\nfoo"
-echo "$LINES" | tail -n +2 | while read LINE
-                             do
-                                 THISCLUSTER=$(echo "$LINE" | cut -f 1)
-                                 THISNAME=$(echo "$LINE"    | cut -f 2)
-                                 # Start a new line if we've hit a new cluster
-                                 if [[ "x$THISCLUSTER" = "x$CLUSTER" ]]
-                                 then
-                                     printf "	$THISNAME"
-                                 else
-                                     printf "\n$THISNAME"
-                                 fi
-                                 CLUSTER="$THISCLUSTER"
-                             done
+# Reduce an array of ASTs into an object {cluster1: [...], cluster2: [...], ...}
+# Then convert that into an array of clusters (since the keys are meaningless)
+jq 'reduce .[] as $ast ({}; .[$ast.cluster] += [$ast]) | .[]'
