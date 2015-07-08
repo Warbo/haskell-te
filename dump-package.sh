89c7994b85b2d51a933cbe9e5608a0654a0b8d70
Chris Warburton <chriswarbo@gmail.com>
2015-07-08 12:58:20 +0100

    Do the bare minimum in runPlugin

--- dump-package.sh
+++ dump-package.sh
@@ -3,16 +3,6 @@ set -e
 
 # Extract ASTs from a Cabal package
 
-[[ "$#" -eq 0 ]] && echo "Please specify a Cabal project directory" && exit 1
-
-DIR="$1"
-
-PHASE="all"
-[[ "$#" -gt 1 ]] && PHASE="$2"
-
-RELBASE=$(dirname "$0")
-BASE=$(readlink -f "$RELBASE")
-
 function packageName {
     (shopt -s nullglob
      for CBL in "$DIR"/*.cabal
@@ -21,11 +11,14 @@ function packageName {
      done)
 }
 
-function extractAsts {
-    PKG=$(packageName)
-    (cd "$DIR";
-     nix-shell -E "with import <nixpkgs> {}; ghcWithPlugin \"$PKG\"" \
-               --run "$BASE/runPlugin.sh $PHASE")
-}
+[[ "$#" -eq 0 ]] && echo "Please specify a Cabal project directory" && exit 1
 
-extractAsts
+DIR="$1"
+
+RELBASE=$(dirname "$0")
+BASE=$(readlink -f "$RELBASE")
+
+PKG=$(packageName)
+(cd "$DIR";
+ nix-shell -E "with import <nixpkgs> {}; ghcWithPlugin \"$PKG\"" \
+           --run "$BASE/runPlugin.sh")
