3ca08029ac29cd192a1e6432d388e01035869844
Chris Warburton <chriswarbo@gmail.com>
2015-07-08 13:07:49 +0100

    Arities work

--- getArities.sh
+++ getArities.sh
@@ -1,15 +1,17 @@
-function getArities {
-    getTypes                             |
-        grep 'Test\.QuickSpec\.fun. ""' |
-        while IFS= read -r LINE
-        do
-            NAME=$(echo "$LINE" |
-                          grep -o '" (.*) ::' |
-                          grep -o '(.*)'      |
-                          grep -o '[^(].*[^)]')
-            ARITY=$(echo "$LINE"                                 |
-                           grep -o 'Test\.QuickSpec\.fun[0-9] "' |
-                           grep -o '[0-9]')
-            tagArity "$NAME" "$ARITY"
-        done
-}
+grep 'Test\.QuickSpec\.fun. ""' |
+    while IFS= read -r LINE
+    do
+        RNAME=$(echo "$LINE"                |
+                       grep -o '"[ ]*(.*)[ ]*::'  |
+                       grep -o '(.*)'       |
+                       grep -o '[^(].*[^)]' |
+                       rev)
+        NAME=$(echo "$RNAME" | cut -d '.' -f 1  | rev)
+        MODS=$(echo "$RNAME" | cut -d '.' -f 2- | rev)
+
+        ARITY=$(echo "$LINE"                                 |
+                       grep -o 'Test\.QuickSpec\.fun[0-9] "' |
+                       grep -o '[0-9]')
+
+        echo "{\"module\": \"$MODS\", \"name\": \"$NAME\", \"arity\": \"$ARITY\"}"
+    done
