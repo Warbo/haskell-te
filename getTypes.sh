418113f170c2e9851f10a8c82724e67fd6cc0b79
Chris Warburton <chriswarbo@gmail.com>
2015-07-11 00:21:32 +0100

    Fixes

--- getTypes.sh
+++ getTypes.sh
@@ -1,25 +1,22 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p jq
+
+# Monomorphic types come in via stdin
 
 function trim {
     grep -o '[^ ].*[^ ]'
 }
 
-grep 'in f[ ]*::'                   |
-    grep -o '([^)]*)) in f[ ]*::.*' |
-    sed -e "s/::/\t/g"              |
+# Turn (foo)::bar::baz into foo\tbaz
+grep '::' | sed 's/(\(.*\)).*::*.*::\(.*\)/\1\t\2/g' |
     while read -r LINE
     do
-        TERM=$(echo "$LINE" | cut -f 1 | trim)
-        TYPE=$(echo "$LINE" | cut -f 2 | trim)
+        # Cut at the \t, trim whitespace and reverse the (qualified) name
+        RNAME=$(echo "$LINE" | cut -f 1 | trim | rev)
+        TYPE=$( echo "$LINE" | cut -f 2 | trim)
 
-        # $TYPE is fine as-is; $TERM needs cutting down
-        RNAME=$(echo "$TERM" | grep -o "('[^)]*)) in f" |
-                               grep -o "'.*)) in "      |
-                               cut -c 2-                |
-                               rev | cut -c 7-)
-
-        # Reverse 'Mod1.Mod2.name' to get 'eman.2doM.1doM', chop off the
-        # first field, then reverse again to get 'Mod1.Mod2' and 'name'
+        # Chop the reversed name at the first dot, eg. 'eman.2doM.1doM' gives
+        # 'eman' and '2doM.1doM', then reverse to get 'name' and 'Mod1.Mod2'
         NAME=$(echo "$RNAME" | cut -d '.' -f 1  | rev)
         MODS=$(echo "$RNAME" | cut -d '.' -f 2- | rev)
 
