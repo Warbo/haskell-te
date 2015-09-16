283c2f9b383a3a7696c69a848bb672548c09d026
Chris Warburton <chriswarbo@gmail.com>
2015-09-16 11:19:55 +0100

    Switched out TreeFeatures for ML4HSFE (no recursion yet)

--- extractFeatures.sh
+++ extractFeatures.sh
@@ -1,16 +1,30 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -p treefeatures -p bash -p jq -i bash
+#! nix-shell -i bash -p ML4HSFE bash jq
 
 source common.sh
 
-jq -c '.[]' |
-    while read -r LINE
-    do
-        # Extract the "ast" value and pipe into TreeFeatures
-        FEATURES=$(echo "$LINE" |
-                   jq -r '.ast' |
-                   BITS=30 MODE=sexpr TreeFeatures)
+# Arbitrary sizes for matrices
+WIDTH=10
+HEIGHT=10
+export WIDTH
+export HEIGHT
 
-        # Add the features to the object
-        echo "$LINE" | jq -c ". + {features: \"$FEATURES\"}"
-    done | jq -s '.'
+# Read in JSON array of all definitions
+ALL=$(cat)
+
+# Split up the array into lines and loop over them
+echo "$ALL" | jq -c '.[]' | while read -r LINE
+do
+    # Extract the "ast" value for this entry
+    AST=$(echo "$LINE" | jq -r '.ast')
+    export AST
+
+    # Call ML4HSFE, which will do feature-extraction on "AST". We send $ALL on
+    # stdin so that ML4HSFE can look up previous cluster results.
+    FEATURES=$(echo "$ALL" | ML4HSFE)
+
+    # Add the features to the object
+    echo "$LINE" | jq -c ". + {features: \"$FEATURES\"}"
+
+# "Slurp" up the lines into another array
+done | jq -s '.'
