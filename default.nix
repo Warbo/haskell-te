6de86f7db26835be63b330f622ca1eb6e01d3eb8
Chris Warburton <chriswarbo@gmail.com>
2015-11-20 16:28:48 +0000

    Fetch latestGit from chriswarbo.net rather than duplicating

--- default.nix
+++ default.nix
@@ -1,14 +1,12 @@
 # Theory Exploration tools for Haskell; packaged for Nix
 
 # Take haskellPackages as a parameter. Also accept src overrides (see addPkg)
-args@{ haskellPackages, lib, ... }:
+args@{ haskellPackages, lib, latestGit ? (import ./latestGit.nix), ... }:
 
 # Attributes are package names, "call" is whether to use callPackage or just
 # import. "url" is the default Git repo name which, annoyingly, doesn't always
-# match the package name. Fixable, but would # temporarily break everything ;)
-let
-#rec {
- urls = {
+# match the package name. Fixable, but would temporarily break everything ;)
+let urls = {
       ArbitraryHaskell = { call = true;  url = "arbitrary-haskell"; };
       AstPlugin        = { call = true;  url = "ast-plugin";        };
       getDeps          = { call = true;  url = "get-deps";          };
@@ -22,14 +20,10 @@ let
       treefeatures     = { call = true;  url = "tree-features";     };
     };
 
-    # Uses the current HEAD of a git repo, rather than a fixed commit ID
-    latestGit = import ./latestGit.nix;
-
     # Default source for TE tools
-    getGit = x: let path = urls.${x}.url;
-                 in latestGit {
-                      url = "http://chriswarbo.net/git/${path}.git";
-                    };
+    getGit = x: latestGit {
+                  url = "http://chriswarbo.net/git/${urls.${x}.url}.git";
+                };
 
     # Append package "x" to the set "p"
     addPkg = hs: x: p: let source  = args.${x} or (getGit x);
@@ -39,13 +33,10 @@ let
                     in p // { ${x} = package; };
 
 # Return a modified set of haskellPackages, with added theory exploration tools
-in
- #x =
- haskellPackages.override {
+in haskellPackages.override {
 
   # Build up a set of overrides, by mapping over the attributes in urls
   overrides = self: super: lib.fold (addPkg self)
                                     {}
                                     (builtins.attrNames urls);
-#};
 }
