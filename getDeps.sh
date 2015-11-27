a67345e11be5cbf8c5a9ef51fdbd0515b750bb36
Chris Warburton <chriswarbo@gmail.com>
2015-11-27 14:33:28 +0000

    No need for common.sh

---
+++ getDeps.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -p getdeps -p bash -p jq -i bash
+
+jq -c '.[]'
+while read -r LINE
+do
+    # Extract the "ast" value and pipe into TreeFeatures
+    DEPENDENCIES=$(echo "$LINE" | jq -r '.ast' | GetDeps )
+    [[ -z "$DEPENDENCIES" ]] && echo "LINE: $LINE" >> /dev/stderr
+
+    # Add the features to the object
+    echo "$LINE" | jq -c ". + {\"dependencies\": $DEPENDENCIES }"
+done  | jq -s '.'
