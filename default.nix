a5653bf042d43185a989c5ea6b5cf2ffbc9e12fb
Chris Warburton <chriswarbo@gmail.com>
2016-02-01 10:57:57 +0000

    Add Nix pkg

---
+++ default.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation {
+  name = "cabal2db";
+
+  # Exclude .git and test-data from being imported into the Nix store
+  src = builtins.filterSource (path: type:
+    baseNameOf path != ".git" &&
+    baseNameOf path != "test-data") ./.;
+
+  installPhase = ''
+    mkdir -p "$out/bin"
+    cp dump-hackage "$out/bin/"
+    cp runAstPlugin "$out/bin/"
+
+    mkdir -p "$out/lib"
+    cp ghcWithPlugin.nix "$out/lib/"
+
+    echo "Patching to use '$out/lib/ghcWithPlugin.nix'"
+    sed -e "s@./ghcWithPlugin.nix@$out/lib/ghcWithPlugin.nix@g" < dump-package > "$out/bin/dump-package"
+
+    chmod +x "$out/bin/"*
+  '';
+}
