025e1a5cf75189f883ba4b061e7376a7614de8bf
Chris Warburton <chriswarbo@gmail.com>
2016-02-01 19:47:12 +0000

    Don't rely on working dir

--- nix_recurrentClustering.sh
+++ nix_recurrentClustering.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+BASE_=$(dirname "$0")
+BASE=$(readlink -f "$BASE_")
+
+if [[ -e "$BASE/weka-cli.nix" ]]
+then
+    WEKACLI="$BASE/weka-cli.nix"
+elif [[ -e "$BASE/../lib/weka-cli.nix" ]]
+then
+    WEKACLI="$BASE/../lib/weka-cli.nix"
+else
+    echo "Cannot find weka-cli.nix in '$BASE' or '$BASE/../lib/'" >> /dev/stderr
+    exit 1
+fi
+
 # nix-shell shebangs don't like whitespace, so we call nix-shell explicitly
-nix-shell -p 'import ./weka-cli.nix' jq bash order-deps perl \
-          --run "./recurrentClustering.sh"
+nix-shell -p "import \"$WEKACLI\"" jq bash order-deps perl \
+          --run "$BASE/recurrentClustering"
