4c77d6d5014d7992a4fb1ae6ccbc910c3a3882bf
Chris Warburton <chriswarbo@gmail.com>
2016-02-18 14:33:08 +0000

    Make sure scripts are put in bin

--- default.nix
+++ default.nix
@@ -5,6 +5,10 @@ stdenv.mkDerivation {
   src = ./.;
   installPhase = ''
     mkdir -p "$out/bin"
-    cp -v explore-theories "$out/bin/"
+    for F in build-env explore-theories extra-haskell-packages extra-packages path-to-front
+    do
+        cp -v "$F" "$out/bin/"
+        chmod +x "$out/bin/$F"
+    done
   '';
 }
