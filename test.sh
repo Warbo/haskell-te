e8bf6173a31a9934fe856294b76b1fc234352099
Chris Warburton <chriswarbo@gmail.com>
2016-03-01 01:26:02 +0000

    Improve testing

--- test.sh
+++ test.sh
@@ -18,6 +18,49 @@ function fail {
 
 # Test data
 
+function buildable {
+    # Test that the given package can be built normally; let alone with our
+    # modifications
+    F="$TESTDATA/$1.buildable"
+    [[ ! -e "$F" ]] && {
+        BUILDABLE=1  # Assume failure
+        TMP="$TESTDATA/building"
+        mkdir -v "$TMP"
+        pushd "$TMP" > /dev/null
+        if cabal get "$1"
+        then
+            cd "$1"*
+            if nix-shell --run "cabal configure && cabal build" \
+                 -E "with import <nixpkgs> {}; import \"$GHCWITHPLUGIN\" \"$1\""
+            then
+                BUILDABLE=0 # Success
+            else
+                echo "Couldn't configure/build '$1'" >> /dev/stderr
+            fi
+        else
+            echo "Couldn't fetch '$1'" >> /dev/stderr
+        fi
+        popd > /dev/null
+        rm -r "$TMP"
+        echo "$BUILDABLE" > "$F"
+    }
+    BUILDABLE=$(cat "$F")
+    return "$BUILDABLE"
+}
+
+function ghcWithPlugin {
+    if [[ -e "$BASE/ghcWithPlugin.nix" ]]
+    then
+        echo "$BASE/ghcWithPlugin.nix"
+    elif [[ -e "$BASE/../lib/ghcWithPlugin.nix" ]]
+    then
+        echo "$BASE/../lib/ghcWithPlugin.nix"
+    else
+        fail "Didn't find ghcWithPlugin.nix in '$BASE' or '$BASE/../lib'"
+        exit 1
+    fi
+}
+
 function getRawJson {
     # Takes a package name, dumps its ASTs into $TESTDATA
     F="$TESTDATA/$1.rawjson"
@@ -63,6 +106,16 @@ function getTests {
     # Get a list of all test functions
     getFunctions | grep '^test'
 
+    TESTPKGS=$(getTestPkgs) || {
+        fail "Problem getting list of packages to test, aborting"
+        exit 1
+    }
+
+    [[ -n "$TESTPKGS" ]] || {
+        fail "Wasn't able to find any working packages to test with, aborting"
+        exit 1
+    }
+
     # Apply each package test to each package
     while read -r pkg
     do
@@ -70,14 +123,21 @@ function getTests {
         do
             echo "$test $pkg"
         done < <(getPkgTests)
-    done < <(getTestPkgs)
+    done < <(echo "$TESTPKGS")
 }
 
 function getTestPkgs {
     # A list of packages to test with
-    cat <<EOF
-list-extras
-EOF
+    for PKG in list-extras xmonad pandoc git-annex hakyll egison lens warp conduit ghc-mod shelly http-conduit yesod-core
+    do
+        if buildable "$PKG" >> /dev/stderr
+        then
+            echo "Running tests for '$PKG', as it is buildable" >> /dev/stderr
+            echo "$PKG"
+        else
+            echo "Skipping '$PKG' as it couldn't be built" >> /dev/stderr
+        fi
+    done
 }
 
 function traceTest {
@@ -111,6 +171,9 @@ function runTests {
     # Overall script exit code
     CODE=0
 
+    # Set up directories, etc.
+    init
+
     # Handle a regex, if we've been given one
     if [[ -z "$1" ]]
     then
@@ -118,9 +181,6 @@ function runTests {
     else
         TESTS=$(getTests | grep "$1")
     fi
-
-    # Set up directories, etc.
-    init
 
     while read -r test
     do
@@ -155,6 +215,9 @@ function init() {
     echo "Attempting to create test data dir '$TESTDATA'" >> /dev/stderr
     mkdir -p "$TESTDATA/debug" ||
         abort "Couldn't make test data dir '$TESTDATA'"
+
+    # Look for ghcWithPlugin.nix
+    GHCWITHPLUGIN=$(readlink -f "$(ghcWithPlugin)")
 }
 
 function cleanup() {
