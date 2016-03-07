ceeae5a5928860f08436f29a63da3c0ed0a90ad8
Chris Warburton <chriswarbo@gmail.com>
2016-03-07 09:22:18 +0000

    Sped up shufflePackages.sh

--- shufflePackages.sh
+++ shufflePackages.sh
@@ -1,24 +1,57 @@
 #!/usr/bin/env bash
+DIR=$("$BASE/cacheDir.sh")
 if [[ -n "$1" ]]
 then
     F="$1"
+elif [[ -f "$DIR/index.tar.gz" ]]
+then
+    F="$DIR/index.tar.gz"
 else
-    F="index.tar.gz"
+    URL="http://hackage.haskell.org/packages/index.tar.gz"
+    echo "Downloading Hackage package list from '$URL'" >> /dev/stderr
+    wget -O "$DIR/index.tar.gz"  || {
+        echo "Failed to fetch package list, aborting" >> /dev/stderr
+        exit 1
+    }
 fi
 
-function randomNames {
-    tar -ztf "$F" | cut -d / -f 1 | sort -u | shuf
-}
+BASE=$(dirname "$(readlink -f "$0")")
 
-function version {
-    tar -ztf "$F" | grep "^$1/" | tail -n1 | cut -d / -f 1-2 | tr / -
+function extractVersions {
+    PKG=""
+    LATEST=""
+    while read -r LINE
+    do
+        THIS_PKG=$(echo "$LINE" | cut -d / -f 1)
+        if ! [[ "x$PKG" = "x$THIS_PKG" ]]
+        then
+            # Reached a new package; report the latest version of the last one
+            [[ -z "$PKG" ]] || echo -e "$PKG\t$LATEST"
+
+            # Start tracking this package instead
+            PKG="$THIS_PKG"
+        fi
+
+        # Bump the latest version we've seen
+        LATEST=$(echo "$LINE" | cut -d / -f 2)
+    done < <(tar -ztf "$F" | grep -o '[^/][^/]*/[0-9][^/]*')
+    echo -e "$PKG\t$LATEST"
 }
 
 function withVersions {
-    while read -r PKG
-    do
-        version "$PKG"
-    done < <(randomNames)
+    HASH=$(md5sum "$F" | cut -d ' ' -f 1)
+    CACHED="$DIR/package-versions-$HASH"
+    if [[ -f "$CACHED" ]]
+    then
+        cat "$CACHED"
+    else
+        echo "Caching package versions to '$CACHED'" >> /dev/stderr
+        extractVersions | tee "$CACHED"
+    fi
 }
 
-withVersions
+function randomPkgs {
+    withVersions | uniq | shuf
+}
+
+randomPkgs
