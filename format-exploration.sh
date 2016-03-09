f784a4eeab4e0644a15383fb2a79e916f778ee97
Chris Warburton <chriswarbo@gmail.com>
2016-03-09 20:08:12 +0000

    Only explore definitions with types

--- format-exploration.sh
+++ format-exploration.sh
@@ -17,9 +17,10 @@ function individualClusters {
     # shellcheck disable=SC2153
     for CLUSTER in $(seq 1 "$CLUSTERS")
     do
-        # Select entries which have a "cluster" attribute matching $CLUSTER, and
-        # a "quickspecable" attribute which is true
-        GOT=$(echo "$INPUT" | jq -c "map(select(.cluster == $CLUSTER and .quickspecable))")
+        # Select entries which have a "cluster" attribute matching $CLUSTER, a
+        # non-null "type" attribute and a true "quickspecable" attribute.
+        GOT=$(echo "$INPUT" | jq -c \
+            "map(select(.cluster == $CLUSTER and .type != null and .quickspecable))")
 
         # Only output non-empty clusters
         LENGTH=$(echo "$GOT" | jq 'length')
