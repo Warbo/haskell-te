f762cfe988f5b54f4246dc944d3dc373eee4bcbd
Chris Warburton <chriswarbo@gmail.com>
2016-03-27 15:29:42 +0100

    Use hash properly

--- dumpToNix.nix
+++ dumpToNix.nix
@@ -3,7 +3,7 @@ pkgDir:
 
 stdenv.mkDerivation {
   inherit pkgDir;
-  name = "dump-to-nix-${builtins.hashString pkgDir}";
+  name = "dump-to-nix-${builtins.hashString "sha256" pkgDir}";
   buildInputs = [ cabal2db ];
   builder = ''
     dump-package "$pkgDir" > "$out"
