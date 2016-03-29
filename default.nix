3411bdd67df61965737ca515aa63af8f31d9ea03
Chris Warburton <chriswarbo@gmail.com>
2016-03-29 14:01:54 +0100

    Try to keep cabal2db as top-level

--- default.nix
+++ default.nix
@@ -1,3 +1,37 @@
-{ stdenv, haskellPackages, nix, jq }:
+{ stdenv, nix, haskellPackages, jq }:
 
-(import ./defs.nix { inherit stdenv haskellPackages nix jq; }).cabal2db
+stdenv.mkDerivation {
+  name = "cabal2db";
+
+  # Exclude .git from being imported into the Nix store
+  src = builtins.filterSource (path: type:
+    baseNameOf path != ".git") ./.;
+
+  propagatedBuildInputs = [ nix haskellPackages.cabal-install jq ];
+
+  NIX_REMOTE = "daemon";
+  NIX_PATH = builtins.getEnv "NIX_PATH";
+  doCheck = builtins.getEnv "NIX_DO_CHECK" != "0";
+  checkPhase = ''
+    echo "Running $PWD/test.sh" 1>&2
+    ./test.sh
+  '';
+
+  installPhase = ''
+    mkdir -p "$out/bin"
+
+    for CMD in dump-format dump-hackage dump-package dump-package-env \
+               dump-package-name runAstPlugin
+    do
+        cp -v "$CMD" "$out/bin/"
+    done
+
+    mkdir -p "$out/lib"
+    for F in *.nix
+    do
+        cp -v "$F" "$out/lib/"
+    done
+
+    chmod +x "$out/bin/"*
+  '';
+}
--- defs.nix
+++ defs.nix
@@ -1,7 +1,7 @@
-{ stdenv, haskellPackages, nix, jq }:
+{ stdenv, haskellPackages, nix, jq, cabal2db }:
 
 rec {
-  cabal2db        = import ./cabal2db.nix        { inherit stdenv haskellPackages nix jq; };
+  #cabal2db        = import ./cabal2db.nix        { inherit stdenv haskellPackages nix jq; };
   downloadToNix   = import ./downloadToNix.nix   { inherit stdenv haskellPackages;        };
   dumpToNix       = import ./dumpToNix.nix       { inherit stdenv cabal2db;               };
   build           = import ./build.nix           { inherit stdenv haskellPackages;        };
