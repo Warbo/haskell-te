c1e9efffc6c3c497a5644366736b539c047ea95d
Chris Warburton <chriswarbo@gmail.com>
2016-04-02 21:20:16 +0100

    Rename nix-test files to test

--- nix-support/test.nix
+++ nix-support/test.nix
@@ -1,11 +1,11 @@
 # Import useful stuff
 let pkgs = import <nixpkgs> {};
-    defs = pkgs // pkgs.lib // builtins // import ./nix-support {};
+    defs = pkgs // pkgs.lib // builtins // import ./. {};
  in with defs;
 
 # Import all *.nix files from ./tests, pass defs to each and assert that they
 # return true
-let tests   = importDir ./nix-tests;
+let tests   = importDir ../tests;
     runTest = name: let msg = "Running test '${name}'";
                      in trace msg (assertMsg (tests."${name}" defs) msg);
  in all runTest (attrNames tests)
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 BASE=$(dirname "$(readlink -f "$0")")
 NIX_PATH="$("$BASE/nix-support/nixPath.sh")" nix-instantiate \
-    --read-write-mode --show-trace --eval -E 'import ./test.nix'
+    --read-write-mode --show-trace --eval -E 'import ./nix-support/test.nix'
