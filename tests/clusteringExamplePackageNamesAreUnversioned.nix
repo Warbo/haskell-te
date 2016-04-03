6b39f7114df742f843e341abdbf7f685dbbaae70
Chris Warburton <chriswarbo@gmail.com>
2016-04-03 23:15:43 +0100

    More tests

---
+++ tests/clusteringExamplePackageNamesAreUnversioned.nix
@@ -0,0 +1,34 @@
+defs: with defs;
+
+let examples = map (f: ./clusteringExamples + "/${f}")
+                   (builtins.attrNames (builtins.readDir ./clusteringExamples));
+    valid    = f: assertMsg (parseJSON (runScript { buildInputs = [ jq ]; } ''
+                 #!/usr/bin/env bash
+                 set -e
+                 function assertNoVersions {
+                   if grep -- "-[0-9][0-9.]*$" > /dev/null
+                   then
+                     echo "Versions found in package names of $1${f}" 1>&2
+                     exit 1
+                   fi
+                 }
+
+                 echo "Checking '${f}'" 1>&2
+
+                 echo "Parsing '${f}'" 1>&2
+                 jq '.' < "${f}" > /dev/null || {
+                   echo "Failed to parse '${f}'" 1>&2
+                   exit 2
+                 }
+
+                 echo "Checking package names of '${f}'" 1>&2
+                 jq -rc '.[] | .package'                       < "${f}" |
+                   assertNoVersions ""
+
+                 echo "Checking package names of '${f}'" 1>&2
+                 jq -rc '.[] | .dependencies | .[] | .package' < "${f}" |
+                   assertNoVersions "dependencies of "
+
+                 echo "true" > "$out"
+               '')) "Checking for versioned package names in '${f}'";
+ in all valid examples
--- tests/clusteringExamplesHaveFeatures.nix
+++ tests/clusteringExamplesHaveFeatures.nix
@@ -2,7 +2,7 @@ defs: with defs;
 
 let examples = map (f: ./clusteringExamples + "/${f}")
                    (builtins.attrNames (builtins.readDir ./clusteringExamples));
-                   count = f: parseJSON (runScript { buildInputs = [ jq ml4hsfe ]; } ''
+                   count = f: parseJSON (runScript { buildInputs = [ jq ML4HSFE ]; } ''
                              set -e
                              #ENAME=$(basename "${f}")
                              WIDTH=30 HEIGHT=30 ml4hsfe-loop < "${f}" |
