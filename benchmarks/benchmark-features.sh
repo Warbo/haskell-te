97c7d2e69fbda036c7ce3e48582621ece4d3664f
Chris Warburton <chriswarbo@gmail.com>
2016-04-03 19:59:33 +0100

    Remove more benchmark scripts

--- benchmarks/benchmark-features.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-BASE=$(dirname "$(dirname "$(readlink -f "$0")")")
-
-"$BASE/benchmarks/benchmark-annotate.sh"  "$@" || {
-    echo "benchmark-annotate.sh failed" >> /dev/stderr
-    exit 1
-}
-
-exit 0
