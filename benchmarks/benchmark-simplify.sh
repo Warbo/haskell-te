41c49da98d730518f7ad0e93fc63ed7e9476605a
Chris Warburton <chriswarbo@gmail.com>
2016-04-05 01:39:56 +0100

    Remove dummy benchmark

--- benchmarks/benchmark-simplify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$0: not implemented yet" >> /dev/stderr
-exit 0
-
-#while read -r CLUSTERS
-#do
-#    benchmark simplify equations from CLUSTERS output
-#done < <(clusterList)
