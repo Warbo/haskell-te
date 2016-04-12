67021b6c69261241dd0e4760edf37737ae1c280e
Chris Warburton <chriswarbo@gmail.com>
2016-04-12 19:24:55 +0100

    Adjusting to lists

--- tests/pkgTests/onlyQuickspecableFormatted.nix
+++ tests/pkgTests/onlyQuickspecableFormatted.nix
@@ -1,9 +1,16 @@
 defs: with defs; pkg:
+with builtins;
 with lib;
 
-let check = c: data: parseJSON (runScript { buildInputs = [ jq ]; } ''
-      set -e
-      jq 'map(.[] | .quickspecable) | all' < "${data}" > "$out"
-    '');
-    checkAll = mapAttrs check pkg.formatted;
- in all (c: checkAll."${toString c}") defaultClusters
+let
+
+check = data:
+  let result = parseJSON (runScript { buildInputs = [ jq ]; } ''
+        set -e
+        jq 'map(.quickspecable) | all' < "${data}" > "$out"
+      '');
+   in testMsg result "Ensuring all quickspecable ${toJSON data}";
+
+checkAll = mapAttrs (_: all check) pkg.formatted;
+
+in all (c: checkAll.${toString c}) defaultClusters
