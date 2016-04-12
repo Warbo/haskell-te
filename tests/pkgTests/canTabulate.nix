76d18143a93d451320daa188e5c3289b92061cd2
Chris Warburton <chriswarbo@gmail.com>
2016-04-12 18:00:17 +0100

    Allow each x value to have multiple y values

--- tests/pkgTests/canTabulate.nix
+++ tests/pkgTests/canTabulate.nix
@@ -4,12 +4,40 @@ with lib;
 with tabulate { clusters = defaultClusters; quick = false; };
 
 # Checking the value types forces them to be evaluated
-let check = tbl: addErrorContext "Checking '${toJSON tbl}'"
-      (with tbl; all id [
-        (testMsg (isList matrix)             "Table body is a list")
-        (testMsg (all isList matrix)         "Table rows are lists")
-        (testMsg (all (all isString) matrix) "Table cells contain strings")
-      ]);
- in all check [
-      (eqsVsTimeForClusters pkg.sizeDataPoints)
-    ]
+let
+
+sensibleStdDev = x: x ? stddev -> all id [
+  (isAttrs x.stddev)
+  (x.stddev ? estPoint)
+  (isString x.stddev.estPoint)
+];
+
+isTime = x: all id [
+  (isAttrs x)
+  (x ? mean)
+  (x.mean ? estPoint)
+  (sensibleStdDev x)
+];
+
+hasValues = x: testMsg (isList x && all isValue x) "${toJSON x} hasValues";
+
+isValue = x: testMsg (isString x || isTime x)
+                     "isValue ${toJSON x}";
+
+checkTable = tbl: addErrorContext "Checking table '${toJSON tbl}'"
+  (with tbl; all id [
+    (testMsg (isList matrix)
+             "Table body is a list ${toJSON matrix}")
+
+    (testMsg (all isList matrix)
+             "Table rows are lists ${toJSON matrix}")
+
+    (testMsg (all (all hasValues) matrix)
+             "Table cells contain values ${toJSON matrix}")
+  ]);
+
+in
+
+all checkTable [
+  (eqsVsTimeForClusters pkg.sizeDataPoints)
+]
