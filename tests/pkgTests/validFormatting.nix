38ba49a7620aa536fe69464fd4949757768faad5
Chris Warburton <chriswarbo@gmail.com>
2016-04-12 22:28:19 +0100

    Don't test old formatting

--- tests/pkgTests/validFormatting.nix
+++ tests/pkgTests/validFormatting.nix
@@ -4,14 +4,10 @@ with lib;
 let check = c: data: parseJSON (runScript { buildInputs = [ jq ]; } ''
       set -e
       LENGTH=$(jq -c 'length' < "${data}") || {
-        echo "Can't read '${c}' formatted clusters for '${pkg.name}'" 1>&2
+        echo "Can't read formatted cluster from '${data}' for '${pkg.name}'" 1>&2
         exit 2
-      }
-      [[ "$LENGTH" -eq "${c}" ]] || [[ "$LENGTH" -lt "${c}" ]] || {
-        echo "Formatted '$LENGTH' clusters for '${pkg.name}' instead of '${c}'" 1>&2
-        exit 3
       }
       echo "true" > "$out"
     '');
-    checkAll = mapAttrs check pkg.formatted;
+    checkAll = mapAttrs (c: v: all (check c) v) pkg.formatted;
  in all (c: checkAll."${toString c}") defaultClusters
