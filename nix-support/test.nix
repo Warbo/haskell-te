40d0c7eb40e793b0763aab225506e33e87586642
Chris Warburton <chriswarbo@gmail.com>
2016-04-13 17:37:28 +0100

    Try to keep going after failures

--- nix-support/test.nix
+++ nix-support/test.nix
@@ -4,7 +4,12 @@ let pkgs = import <nixpkgs> {};
 
 # Import all *.nix files from ./tests, pass pkgs to each and assert that they
 # return true
-let tests   = importDir ../tests;
-    runTest = name: testMsg (tests."${name}" pkgs) "Running test '${name}'";
-    result  = all runTest (attrNames tests);
+let tests       = importDir ../tests;
+    runTest     = name: testMsg (tests."${name}" pkgs) "Running test '${name}'";
+    testResults = map runTest (attrNames tests);
+
+    # Force testResults, so one failing test doesn't short-circuit the stop the
+    # others from being run.
+    result      = assert isString "Forcing ${toJSON testResults}";
+                  all id testResults;
  in testMsg result "All tests passed"
--- tests/pkgTests.nix
+++ tests/pkgTests.nix
@@ -4,11 +4,31 @@ with lib;
 
 # Run the tests in ./pkgTests with testPackages
 
-let tests        = importDir ./pkgTests;
-    runTest      = testName: collate (mapAttrs (runTestOnPkg testName)
-                                               testPackages);
-    runTestOnPkg = testName: pkgName: pkg:
-      testMsg (tests."${testName}" defs pkg)
-              "Running test '${testName}' with Haskell package '${pkgName}'";
-    collate      = xs: all (n: xs."${n}") (attrNames xs);
- in all runTest (attrNames tests)
+let
+
+tests = importDir ./pkgTests;
+
+# Apply the given test to all testPackages
+testOnPkgs = testName: test: mapAttrs (run testName test) testPackages;
+
+# Run the given test on the given package
+run = testName: test: pkgName: pkg:
+        testMsg (test defs pkg) "Testing ${testName} on ${pkgName}";
+
+# All results, with no collation
+resultsPerTestPerPkg = mapAttrs testOnPkgs tests;
+
+# Collate results per test: if a test fails for one package, don't keep trying
+# it on others
+resultsPerTest = mapAttrs (_: results: all id (attrValues results))
+                          resultsPerTestPerPkg;
+
+# Strip test names, since they're in our assertion messages anyway
+resultList = attrValues resultsPerTest;
+
+in
+
+# Force all test results, to avoid one failing test short-circuiting the rest
+assert isString "Forcing ${toJSON resultList}";
+
+all id resultList
