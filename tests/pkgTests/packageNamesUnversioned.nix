ac9953665b37dc93c47eed9f7371b8f650bed57a
Chris Warburton <chriswarbo@gmail.com>
2016-05-03 11:42:35 +0100

    Fix broken path variable

--- tests/pkgTests/packageNamesUnversioned.nix
+++ tests/pkgTests/packageNamesUnversioned.nix
@@ -6,16 +6,20 @@ parseJSON (runScript {} ''
   function assertNoVersions {
     if grep -- "-[0-9][0-9.]*$" > /dev/null
     then
-         echo "Versions found in package names of $1${pkg.name}" 1>&2
-         exit 1
+      echo "Versions found in package names of $1${pkg.name}" 1>&2
+      exit 1
     fi
   }
 
-
-  jq -rc '.[] | .package'                       < "{pkg.annotated}" |
+  F="${pkg.annotated}"
+  [[ -e "$F" ]] || {
+    echo "Couldn't find file '$F'" 1>&2
+    exit 1
+  }
+  jq -rc '.[] | .package'                       < "$F" |
     assertNoVersions "'$1'"
 
-  jq -rc '.[] | .dependencies | .[] | .package' < "{pkg.annotated}" |
+  jq -rc '.[] | .dependencies | .[] | .package' < "$F" |
     assertNoVersions "dependencies of '$1'"
 
   echo "true" > "$out"
