74ebb897d78ce4c28b9e6bb50f9c41cb3de58a29
Chris Warburton <chriswarbo@gmail.com>
2016-06-15 13:21:55 +0100

    Reproduce and check for runTypesScript error

--- tests/annotateTipPackage.nix
+++ tests/annotateTipPackage.nix
@@ -1,4 +1,78 @@
 defs: with defs; with builtins;
 
-let processed = tipBenchmarks.process { quick = true; };
- in trace (toJSON processed.rawAnnotated) false
+let
+
+# Check the package itself
+pkg = tipBenchmarks.pkg;
+
+haveSrc = testMsg (pathExists pkg.src) "Tip module source exists";
+
+# Make sure we can dump the ASTs
+quick = true;
+
+dump = dumpPackage { inherit quick; inherit (pkg) src; };
+
+haveDump = testMsg (!dump.failed)           "Tip module dump succeeded" &&
+           testMsg (pathExists dump.stdout) "Dumped Tip module";
+
+# Annotation is trickier, so these are mainly regressions tests to ensure that
+# our scripts can handle packages taken straight from Cabal directories
+asts      = dump.stdout;
+pkgName   = pkg.name;
+env       = { buildInputs = [ adb-scripts ]; };
+
+ranTypes = parseJSON (runScript env ''
+             set -e
+             "${runTypesScript}" "${pkgName}" < "${asts}" > stdout 2> stderr
+             CODE="$?"
+
+             STDOUT=$(nix-store --add stdout)
+             STDERR=$(nix-store --add stderr)
+
+             printf '{"stdout": "%s", "stderr": "%s", "code": %s}' \
+                     "$STDOUT"       "$STDERR"       "$CODE" > "$out"
+           '');
+
+canRunTypes = testMsg (ranTypes.code == "0")
+                      "Ran runTypesScript on tip module" &&
+              testMsg (match ".*error.*" (readFile ranTypes.stderr) == null)
+                      "No errors in runTypeScript stderr for tip module";
+
+annotatedAsts = runScript env ''
+                  set -e
+                  "${annotateAstsScript}" < "${ranTypes.stdout}" > out
+                  "${storeResult}" out
+                '';
+
+canAnnotateAsts = testMsg ("${annotatedAsts}" != "")
+                          "Can run annotateAstsScript on tip module";
+
+gotDeps = runScript env ''
+            "${getDepsScript}" < "${annotatedAsts}" > out
+            "${storeResult}" out
+          '';
+
+canGetDeps = testMsg ("${gotDeps}" != "")
+                     "Can run getDepsScript on tip module";
+
+# Try the 'annotate' function, which combines the above pieces
+annotated = annotate { inherit quick asts pkgName; };
+
+rawAnnotated = testMsg (!annotated.failed) "Tip module annotation succeeded";
+
+# Just to make sure, also check the encapsulated version we actually use
+processed = tipBenchmarks.process { inherit quick; };
+
+tipAnnotated = testMsg (!processed.rawAnnotated.failed)
+                       "Tip benchmarks annotated";
+
+# Run tests in dependency order
+in all (x: x) [
+     haveSrc
+     haveDump
+     canRunTypes
+     canAnnotateAsts
+     canGetDeps
+     rawAnnotated
+     tipAnnotated
+   ]
