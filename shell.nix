6001a258a4d28ea7ea96e77f069097b30b734265
Chris Warburton <chriswarbo@gmail.com>
2016-06-19 10:50:38 +0100

    Added shell.nix to cache packages

---
+++ shell.nix
@@ -0,0 +1,31 @@
+# Cache packages, so they don't get rebuilt; the line between packages and
+# results is blurry
+let pkgs = import <nixpkgs> {};
+    defs = import ./nix-support pkgs;
+ in pkgs.buildEnv {
+      name  = "haskell-te-deps";
+      paths = with defs; [
+        jq
+        gnutar
+        reduce-equations
+        utillinux
+        getDeps
+        coreutils
+        wget
+        bc
+        ml4hs
+        ML4HSFE
+        weka
+        jre
+        mlspec
+        nix
+        order-deps
+        pv
+        haskellPackages.cabal2nix
+        haskellPackages.cabal-install
+        gnuplot
+        file
+        AstPlugin
+        mlspec-bench
+      ];
+    }
