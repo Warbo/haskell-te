648d169152058b098152e3f15557fdb8c4d93268
Chris Warburton <chriswarbo@gmail.com>
2016-06-20 11:48:53 +0100

    equations isn't raw

--- tests/exploreTip.nix
+++ tests/exploreTip.nix
@@ -8,7 +8,8 @@ failed = output.failed;
 
 eqs = output.equations;
 
-haveEqs = all (n: (readFile eqs."${n}".stdout) != "") (attrNames eqs);
+haveEqs = all (n: (readFile eqs."${n}") != "") (attrNames eqs);
 
-in testMsg (!failed) "TIP benchmark didn't fail" &&
-   testMsg haveEqs "Got equations"
+in addErrorContext "TIP equations: ${toJSON eqs}"
+     (testMsg (!failed) "TIP benchmark didn't fail" &&
+      testMsg haveEqs "Got equations")
