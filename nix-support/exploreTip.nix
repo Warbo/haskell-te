46f47ca9ebc5b42e3473d5b6a2ce0ffaeba4f5fe
Chris Warburton <chriswarbo@gmail.com>
2016-06-22 16:24:40 +0100

    Sample in exploreTip

--- nix-support/exploreTip.nix
+++ nix-support/exploreTip.nix
@@ -1,9 +1,8 @@
 with import ./. {}; with builtins;
 
-let pkgDef = nixFromCabal (toString tipBenchmarks.module) null;
-    cached = haskellPackages.callPackage pkgDef {};
-    pkg    = cached // { inherit currentTime; };
-    result = processPackage { quick = true; } pkg.name pkg;
- in if result.failed
-       then "FAILED"
-       else builtins.attrNames result
+let output = tipBenchmarks.process {
+               clusters = [ 10 ];
+               quick    = true;
+               sampleSize = 10;
+             };
+ in trace (toJSON { inherit (output) rawClustered rawExplored rawReduced; }) "done"
