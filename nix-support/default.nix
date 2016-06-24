58e9635b7d44adac71a992489f5822530d8b25d1
Chris Warburton <chriswarbo@gmail.com>
2016-06-24 08:13:46 +0100

    Remove unnecessary indirection

--- nix-support/default.nix
+++ nix-support/default.nix
@@ -1,26 +1,33 @@
 # Augments <nixpkgs> with our own packages and utilities
 args:
 
-let # When invoking Nix recursively, we want this set to act as <nixpkgs>;
-    # however, that would cause infinite recursion as we'd try to augment
-    # ourselves. To prevent this, we also provide a "<real>" fallback when
-    # calling Nix recursively.
-    pkgs = with builtins; if any (p: p.prefix == "real") nixPath
-                             then import <real>    {}
-                             else import <nixpkgs> {};
+let # When invoking Nix recursively, we want this set to be a pristine clone of
+    # nixpkgs, however that would cause infinite recursion as we'd try to
+    # augment ourselves. To prevent this, we also provide a "<real>" fallback
+    # when calling Nix recursively.
+    pkgs = with builtins;
+           if any (p: p.prefix == "real") nixPath
+              then import <real> {}
+              else import ((import <nixpkgs> {}).fetchFromGitHub {
+                            owner  = "NixOS";
+                            repo   = "nixpkgs";
+                            rev    = "16.03";
+                            sha256 = "0m2b5ignccc5i5cyydhgcgbyl8bqip4dz32gw0c6761pd4kgw56v";
+                          }) {};
+
     # If your <nixpkgs> is heavily customised, you can provide a pristine
     # version to use as 'original.pristine'
-    original = if pkgs ? pristine then pkgs.pristine else pkgs;
+    original = pkgs;
 
     # haskellPackages.override ensures dependencies are overridden too
     haskellPackages = import ./haskellPackages.nix {
-                        inherit (original) haskellPackages;
+                        inherit (pkgs) haskellPackages;
                         nixFromCabal = import ./nixFromCabal.nix {
                                          inherit haskellPackages;
-                                         inherit (original) lib stdenv;
+                                         inherit (pkgs) lib stdenv;
                                        };
                       };
 
-in original // original.callPackage ./defs.nix {
-                 inherit haskellPackages;
-               }
+in pkgs // pkgs.callPackage ./defs.nix {
+             inherit haskellPackages;
+           }
