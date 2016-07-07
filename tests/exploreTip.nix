19d20e4a84f428d2f43a280fa4bf664d3126510e
Chris Warburton <chriswarbo@gmail.com>
2016-07-07 14:40:44 +0100

    Make sure TIP benchmarks DO run out of memory with 1 cluster

--- tests/exploreTip.nix
+++ tests/exploreTip.nix
@@ -2,15 +2,44 @@ defs: with defs; with builtins;
 
 let
 
+memLimKb = (import ../nix-support/timeout.nix {
+             inherit writeScript;
+           }).memLimKb;
+
 strippedContent = f:
   let content  = readFile f;
    in replaceStrings ["\n" " " "\t"]
                      [""   ""  ""]
                      content;
 
-output = tipBenchmarks.process { quick = true; };
+singleClusterFails =
+  let output    = tipBenchmarks.process { quick = true; clusters = [ 1 ]; };
+      explored  = head output.rawExplored.results."1";
+      memUsageS = runScript { buildInputs = [ jq ]; } ''
+                    grep -o "MAXMEM [0-9]*" < "${explored.stderr}" |
+                    grep -o "[0-9]*" > "$out"
+                  '';
+      memUsage  = addErrorContext "Parsing number from '${memUsageS}'"
+                    (fromJSON memUsageS);
+      outOfMem  = memUsage > memLimKb;
+      check     = c: m: testDbg c m (toJSON { inherit output memLimKb; });
+   in all (x: x) [
+        (check output.failed "TIP fails for 1 cluster")
 
-failed = output.failed;
+        (check (!output.rawAnnotated.failed) "TIP annotated for 1 cluster")
+
+        (check (!output.rawDump.failed) "TIP dumped for 1 cluster")
+
+        (check (!output.rawClustered.failed) "TIP clustered for 1 cluster")
+
+        (check output.rawExplored.failed "Can't explore 1 TIP cluster")
+
+        (check explored.failed "Can't explore single TIP cluster")
+
+        (check outOfMem "Exploring TIP ran out of memory on 1 cluster")
+      ];
+
+output = tipBenchmarks.process { quick = true; };
 
 explored = all (n: all (x: strippedContent x != "")
                        output.explored."${n}")
@@ -25,6 +54,8 @@ nonZeroEqs = all (n: output.equationCounts."${n}" > 0)
 withDbg = dbg: msg: x: addErrorContext dbg (testMsg x msg || trace dbg false);
 
 in all (x: x) [
+
+  singleClusterFails
 
   (testDbg (!output.failed)  "TIP benchmark didn't fail"
            (toJSON { inherit (output) rawDump rawAnnotated rawClustered
