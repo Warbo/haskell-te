54c5ca7216dab4d5fea1c5220b61e3af300530c7
Chris Warburton <chriswarbo@gmail.com>
2016-07-20 19:17:16 +0100

    More testing

--- release.nix
+++ release.nix
@@ -1,9 +1,17 @@
 {
   check = with import ./nix-support {};
-          let foo = stdenv.mkDerivation {
+          let drv = writeScript "dummy.nix" ''
+                with import <nixpkgs>;
+                stdenv.mkDerivation {
+                  name = "dummy-input";
+                  buildCommand = "source $stdenv/setup\necho foo > \"$out\"";
+                }
+              '';
+              foo = stdenv.mkDerivation {
                 name = "dummy";
                 buildCommand = ''
                   source $stdenv/setup
+                  mkdir -p "$out"
                   echo "success" > "$out"
                 '';
               };
