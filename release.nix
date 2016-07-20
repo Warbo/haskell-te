32b6a15fdc49839c8fa7309099abd27487cf7df0
Chris Warburton <chriswarbo@gmail.com>
2016-07-20 19:08:37 +0100

    Testing

--- release.nix
+++ release.nix
@@ -1,5 +1,14 @@
 {
-  tests = import ./nix-support/tests.nix {};
+  check = with import ./nix-support {};
+          let foo = stdenv.mkDerivation {
+                name = "dummy";
+                buildCommand = ''
+                  source $stdenv/setup
+                  echo "success" > "$out"
+                '';
+              };
+           in foo;
+  #tests = import ./nix-support/tests.nix {};
 
   #tip-eqs = import ./nix-support/exploreTip.nix;
 }
