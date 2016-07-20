abc2c50a72db542b36abdfb3cf8d3cca410ee643
Chris Warburton <chriswarbo@gmail.com>
2016-07-20 16:35:34 +0100

    Ignore TIP for now, get tests running

--- release.nix
+++ release.nix
@@ -1,5 +1,5 @@
 {
-  tests   = import ./nix-support/tests.nix {};
+  tests = import ./nix-support/tests.nix {};
 
-  tip-eqs = import ./nix-support/exploreTip.nix;
+  #tip-eqs = import ./nix-support/exploreTip.nix;
 }
