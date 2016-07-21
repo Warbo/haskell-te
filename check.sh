591c928b4fbe45d5f86ce7df6a45e50181069da8
Chris Warburton <chriswarbo@gmail.com>
2016-07-21 16:09:19 +0100

    Try instantiating

--- check.sh
+++ check.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-#nix-instantiate --show-trace ./release.nix
+NIX_PATH="$PWD:$NIX_PATH" nix-instantiate \
+  --show-trace \
+  --option restrict-eval true \
+  release.nix
+  #-A tests \
 
-NIX_PATH="$PWD:$NIX_PATH" nix-build --option restrict-eval true --show-trace -A tests release.nix
+#NIX_PATH="$PWD:$NIX_PATH" nix-build --option restrict-eval true --show-trace -A tests release.nix
