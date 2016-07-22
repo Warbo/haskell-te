6f0bbe870df72d0ae548b98b1d8a11ae4740180d
Chris Warburton <chriswarbo@gmail.com>
2016-07-22 14:45:14 +0100

    Make check.sh act like Hydra

--- check.sh
+++ check.sh
@@ -1,9 +1,19 @@
-#!/usr/bin/env bash
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p hydra
 
-NIX_PATH="$PWD:$NIX_PATH" nix-instantiate \
-  --show-trace \
-  --option restrict-eval true \
-  release.nix
-  #-A tests \
+PKG_PATH=$(nix-instantiate --eval -E "<nixpkgs>")
 
-#NIX_PATH="$PWD:$NIX_PATH" nix-build --option restrict-eval true --show-trace -A tests release.nix
+if [[ -n "$NIX_USER_PROFILE_DIR" ]]
+then
+    GC="$NIX_USER_PROFILE_DIR/hydra-roots"
+else
+    GC="/nix/var/nix/gcroots/per-user/$USER/hydra-roots"
+fi
+
+hydra-eval-jobs              \
+    "$PWD/release.nix"       \
+    --gc-roots-dir "$GC"     \
+    -j 1                     \
+    --show-trace             \
+    -I "haskell-te-src=$PWD" \
+    -I "nixpkgs=$PKG_PATH"
