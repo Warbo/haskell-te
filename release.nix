7883c508115984f9886de6d854177f233143ce84
Chris Warburton <chriswarbo@gmail.com>
2016-07-25 23:15:39 +0100

    Build packages too

--- release.nix
+++ release.nix
@@ -3,12 +3,12 @@ let pkgs = import ./nix-support {};
 
   tests = import ./nix-support/tests.nix {};
 
-  #inherit (pkgs.haskellPackages)
-  #  ArbitraryHaskell runtime-arbitrary nix-eval mlspec-helper ifcxt AstPlugin
-  #  getDeps HS2AST ML4HSFE;
+  inherit (pkgs.haskellPackages)
+    ArbitraryHaskell runtime-arbitrary nix-eval mlspec-helper ifcxt AstPlugin
+    getDeps HS2AST ML4HSFE;
 
-  #inherit (pkgs)
-  #  mlspec mlspec-bench order-deps reduce-equations runWeka;
+  inherit (pkgs)
+    mlspec mlspec-bench order-deps reduce-equations runWeka;
 
   #tip-eqs = import ./nix-support/exploreTip.nix;
 }
