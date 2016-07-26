e39d128698a8e9c83b4346198ddbedf6da79d8b1
Chris Warburton <chriswarbo@gmail.com>
2016-07-26 11:09:21 +0100

    Allow tests to return sets of sub-tests

--- nix-support/test.nix
+++ nix-support/test.nix
@@ -2,11 +2,15 @@
 let pkgs = import ./. {};
  in with pkgs; with builtins; with lib;
 
-# Import all *.nix files from ./tests, pass pkgs to each and assert that they
-# return true
+# Import all *.nix files from ./tests, pass pkgs to each and collect up the
+# resulting tests
 let tests       = import ./tests.nix { inherit pkgs; };
     testResults = mapAttrs (n: t: dbug "Running test '${n}'" t)
                            tests;
 
-    result      = attrValues testResults;
+    flatten     = t: if t ? type && t.type == "derivation"
+                        then [ t ]
+                        else concatMap flatten (attrValues t);
+
+    result      = flatten testResults;
  in testWrap result "All tests passed"
