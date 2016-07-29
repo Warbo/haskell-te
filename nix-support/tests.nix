bd7cf48e127b347ef982fdc64708d374ff8d7b2e
Chris Warburton <chriswarbo@gmail.com>
2016-07-29 14:58:10 +0100

    Defer testing to build time, rather than eval time

--- nix-support/tests.nix
+++ nix-support/tests.nix
@@ -3,6 +3,15 @@
 with pkgs.lib;
 with builtins;
 
-let tests  = pkgs.importDir ../tests;
- in mapAttrs (name: test: trace "Testing ${name}" test pkgs)
-             tests
+let tests   = pkgs.importDir ../tests;
+
+    # Our tests are implemented in Nix, so our "builder" is a call to nix-build.
+    # We *could* import the test files directly, but that would cause all of the
+    # tests to run at eval time, which can be frustrating (e.g. getting a list
+    # of the tests would cause all of them to run!)
+    runTest = name: value: pkgs.drvFromScript {} ''
+      cd "${./..}"
+      nix-build --no-out-link -E \
+        'import ./tests/${name}.nix (import ./nix-support {})'
+    '';
+ in mapAttrs runTest tests
