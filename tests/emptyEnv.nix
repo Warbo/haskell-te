1786f38219f995bf63744a5feccdff2d78519674
Chris Warburton <chriswarbo@gmail.com>
2016-07-29 11:56:58 +0100

    Don't parse results unless needed

--- tests/emptyEnv.nix
+++ tests/emptyEnv.nix
@@ -6,17 +6,22 @@ let
 
 f = writeScript "dummy-json" "[]";
 
-result = runScript { buildInputs = explore.extractedEnv null f; } ''
-  set -e
-  while read -r PKG
-  do
-    unset ENVIRONMENT_PACKAGES
-    "${explore.build-env}" ghc-pkg list "$PKG" || {
-      echo "Extra package '$PKG' wasn't found with empty environment" 1>&2
-      exit 2
-    }
-  done < <(echo "${concatStringsSep "\n" explore.extra-haskell-packages}")
-  echo "true" > "$out"
-'';
-
-in testMsg (parseJSON result) "Default env contains extra-haskell-packages"
+in drvFromScript {
+     buildInputs = explore.extractedEnv null f;
+     msg = "Default env contains extra-haskell-packages";
+   }
+   ''
+     set -e
+     while read -r PKG
+     do
+       unset ENVIRONMENT_PACKAGES
+       "${explore.build-env}" ghc-pkg list "$PKG" || {
+         echo "Extra package '$PKG' wasn't found with empty environment" 1>&2
+         echo "not ok - $msg"
+         exit 2
+       }
+     done < <(echo "${concatStringsSep "\n" explore.extra-haskell-packages}")
+     echo "ok - $msg"
+     echo "# $msg" >> "$out
+     echo "# true" >> "$out"
+   ''
