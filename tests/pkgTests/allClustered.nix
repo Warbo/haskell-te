7ee4a28064ea34472c2c78ba84ab7fd2e03650b6
Chris Warburton <chriswarbo@gmail.com>
2016-07-29 21:24:33 +0100

    Fix allClustered

--- tests/pkgTests/allClustered.nix
+++ tests/pkgTests/allClustered.nix
@@ -4,7 +4,7 @@ with builtins;
 let check = c: let sC = toString c;
       in parseJSON (runScript { buildInputs = [ jq ]; } ''
         export CLUSTERS="${sC}"
-        if jq '.[] | .tocluster' < "${pkg.clustered."${sC}"}" |
+        if jq '.[] | .tocluster' < "${pkg.clustered.${sC}}" |
            grep "false" > /dev/null
         then
            echo "Clustering '${pkg.name}' into '${sC}' missed things" 1>&2
@@ -12,4 +12,6 @@ let check = c: let sC = toString c;
         fi
         echo "true" > "$out"
       '');
- in all check defaultClusters
+in listToAttrs (map (c: { name  = toString c;
+                          value = check    c; })
+                    defaultClusters)
