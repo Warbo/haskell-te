682a56d750580504d37f3ae7b00f74041f6687c8
Chris Warburton <chriswarbo@gmail.com>
2016-07-29 22:25:24 +0100

    More fixes

--- tests/pkgTests/allPreClustered.nix
+++ tests/pkgTests/allPreClustered.nix
@@ -5,11 +5,13 @@ let check = c: let sC = toString c; in parseJSON (runScript {
         buildInputs = [ jq ];
     } ''
       export CLUSTERS="${sC}"
-      if jq '.[] | .tocluster' < "${pkg.preClustered."${sC}"}" | grep "false" > /dev/null
+      if jq '.[] | .tocluster' < "${pkg.preClustered.${sC}}" | grep "false" > /dev/null
       then
          echo "Clustering '${pkg.name}' into '${sC}' missed things" 1>&2
          exit 1
       fi
       echo "true" > "$out"
     '');
- in all check defaultClusters
+ in listToAttrs (map (c: { name  = toString c;
+                      value = check c; })
+                     defaultClusters)
