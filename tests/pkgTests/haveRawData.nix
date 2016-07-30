c42343324b539d432175180c81fc583acc121b32
Chris Warburton <chriswarbo@gmail.com>
2016-07-30 01:13:06 +0100

    More fixes

--- tests/pkgTests/haveRawData.nix
+++ tests/pkgTests/haveRawData.nix
@@ -3,4 +3,5 @@ with builtins;
 
 let asts    = downloadAndDump { quick = true; pkgName = pkg.name; };
     rawData = runTypes asts.stdout pkg {};
- in (readFile "${rawData}") != ""
+ in testRun "${pkg.name} has raw data" null { inherit rawData; }
+            ''[[ -n "${rawData}" ]] || exit 1''
