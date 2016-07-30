fe7177d736f9849e354a24f30f6078d20cd44678
Chris Warburton <chriswarbo@gmail.com>
2016-07-30 01:10:05 +0100

    Another fix

--- tests/pkgTests/extractionMatchesHaskell.nix
+++ tests/pkgTests/extractionMatchesHaskell.nix
@@ -2,14 +2,22 @@ defs: with defs; pkg:
 
 # extractFeatures is written in bash + jq, and is really slow. We've
 # replaced it with ml4hsfe-loop, but keep it around for testing
-parseJSON (runScript { buildInputs = [ jq ML4HSFE ]; } ''
+drvFromScript { buildInputs = [ jq ML4HSFE ]; } ''
   set -e
   BASH_RESULT=$("${recurrent-clustering}/lib/extractFeatures" < "${pkg.annotated}" | jq '.') || {
     echo "Couldn't extract features with bash: $BASH_RESULT" 1>&2
     exit 2
   }
 
-  jq -n --argfile bash    <(echo "$BASH_RESULT")    \
-        --argfile haskell "${pkg.features}" \
-        '$bash == $haskell' > "$out"
-'')
+  RESULT=$(jq -n --argfile bash    <(echo "$BASH_RESULT")    \
+              --argfile haskell "${pkg.features}" \
+              '$bash == $haskell')
+
+  if [[ "x$RESULT" = "xtrue" ]]
+  then
+    touch "$out"
+  else
+    echo "RESULT is '$RESULT'" 1>&2
+    exit 1
+  fi
+''
