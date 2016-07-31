3d0da6185bcf682dbd84096e6a21252cc1b7881e
Chris Warburton <chriswarbo@gmail.com>
2016-07-31 16:33:09 +0100

    Add more args

--- nix-support/testPackages.nix
+++ nix-support/testPackages.nix
@@ -1,7 +1,7 @@
 { annotateAstsScript, defaultClusters, getDeps, getDepsScript,
   getTypesScript, jq, lib, ml4hs, ML4HSFE, nixRecurrentClusteringScript,
-  parseJSON, processPackages, runScript, runTypes, runWeka, storeResult,
-  utillinux }:
+  order-deps, parseJSON, processPackages, runScript, runTypes, runWeka,
+  storeResult, utillinux }:
 with builtins;
 with lib;
 
@@ -60,7 +60,7 @@ let clusters         = listToAttrs (map (c: {
       # Like 'clustered', but comes from 'features', rather than using 'cluster'
       # which acts on 'annotated'
       preClustered = mapAttrs (c: _:
-        runScript { buildInputs = [ order-deps ML4HSFE nix jq runWeka ]; } ''
+        runScript { buildInputs = [ order-deps ML4HSFE jq runWeka ]; } ''
           export CLUSTERS="${c}"
           "${nixRecurrentClusteringScript}" < "${features}" > pre-clustered.json
           "${storeResult}" pre-clustered.json "$out"
