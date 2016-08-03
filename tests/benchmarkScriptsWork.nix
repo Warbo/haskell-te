bcd6a8448cb97c3d44910c581e8c8ddabd8d275c
Chris Warburton <chriswarbo@gmail.com>
2016-08-03 18:57:48 +0100

    Check benchmarks handle inputs

--- tests/benchmarkScriptsWork.nix
+++ tests/benchmarkScriptsWork.nix
@@ -28,15 +28,58 @@ let result = script: parseJSON (runScript {
        in testMsg (val == expect)
                     "${field} '${toJSON val}' should be '${toJSON expect}'";
 
-    check = found: testAll [
-              (testField     found "cmd"                         "echo")
-              (testField     found "args"                        ["hello" "world"])
-              (testFieldFile found "stderr"                      "")
-              (testFieldFile found "stdout"                      "hello world\n")
-              (testMsg       (isString found.time.mean.estPoint) "Got mean time")
-            ];
+    check = found: {
+      cmd    = testField     found "cmd"    "echo";
+      args   = testField     found "args"   ["hello" "world"];
+      stderr = testFieldFile found "stderr" "";
+      stdout = testFieldFile found "stdout" "hello world\n";
+
+      time   = testMsg (isString found.time.mean.estPoint) "Got mean time";
+    };
+
+    checkInput = args:
+      let shouldFail = testRun "'benchmark' should abort when packages missing"
+                               null
+                               {}
+                               ''
+                                 if "${benchmark allArgs}"
+                                 then
+                                   echo "'benchmark' didn't spot error" 1>&2
+                                   exit 1
+                                 fi
+                                 touch "$out"
+                               '';
+          shouldSucceed = testRun "'benchmark' works when packages found"
+                                  null
+                                  {}
+                                  ''
+                                    "${benchmark allArgs}" || {
+                                      echo "Benchmark didn't work" 1>&2
+                                      exit 1
+                                    }
+                                  '';
+          allArgs = args // {
+            cmd    = "true";
+            inputs = writeScript "inputs" ''
+                       text
+                       aeson
+                       parsec
+                     ''
+          };
+       in { inherit shouldFail shouldSucceed; };
 
     hasStdDev = testMsg (isString criterionResult.time.stddev.estPoint)
                         "Criterion gives standard deviation";
 
- in testAll [(check timeResult) (check criterionResult) hasStdDev]
+ in {
+   inherit hasStdDev;
+
+   quick = {
+     echo  = check timeResult;
+     input = checkInput { quick = true; };
+   };
+   slow  = {
+     echo  = check criterionResult;
+     input = checkInput { quick = false; };
+   };
+}
