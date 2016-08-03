ec1923865351fba7358332441fe029c75bc8177e
Chris Warburton <chriswarbo@gmail.com>
2016-08-03 15:41:23 +0100

    Fixed typo

--- tests/pkgTests/haveTypeResults.nix
+++ tests/pkgTests/haveTypeResults.nix
@@ -4,7 +4,7 @@ with builtins;
 let result = parseJSON (readFile pkg.ranTypes);
  in {
  haveResult = testMsg (result ? result)
-                      "${pkg.name} has ranTypes.result"
+                      "${pkg.name} has ranTypes.result";
  nonEmpty   = testMsg (strip result.result != "")
                       "${pkg.name} ranTypes.result isn't empty";
 }
