03d8b7a2983d64846ee489c8c600e4e7bc892a6c
Chris Warburton <chriswarbo@gmail.com>
2016-08-05 12:04:55 +0100

    Remove unused scripts

--- annotatedb/scripts.nix
+++
@@ -1,6 +0,0 @@
-{ stdenv, jq, GetDeps, utillinux }:
-
-builtins.trace "FIXME: Get rid of annotatedb/scripts.nix" stdenv.mkDerivation {
-  name = "annotatedb";
-  propagatedBuildInputs = [ jq GetDeps utillinux ];
-}
