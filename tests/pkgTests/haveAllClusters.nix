9ecb25906cc4798b7450ca66dbbabb345748e388
Chris Warburton <chriswarbo@gmail.com>
2016-08-05 12:19:55 +0100

    Add debug info

--- tests/pkgTests/haveAllClusters.nix
+++ tests/pkgTests/haveAllClusters.nix
@@ -6,10 +6,10 @@ let check = c:
           stdin = readFile pkg.clustered."${sC}";
           have  = parseJSON (runScript { buildInputs = [ jq ]; } ''
             set -e
-            FOUND=$(jq '.[] | .cluster' < "${pkg.clustered."${sC}"}")
+            FOUND=$(jq '.[] | .cluster' < "${pkg.clustered.${sC}}")
             for NUM in $(seq 1 "${sC}")
             do
-              echo "$FOUND" | grep "^''${NUM}$" > /dev/null || {
+              echo "$FOUND" | grep "^$NUM$" > /dev/null || {
                 echo "Clustering '${pkg.name}' into '${sC}' clusters, '$NUM' was empty" 1>&2
                 echo "false" > "$out"
               }
@@ -19,5 +19,8 @@ let check = c:
           result = if stdin == ""
                       then trace "Got no stdin" false
                       else have;
-       in testMsg result "Have clusters for ${sC}";
+       in testDbg result "Have clusters for ${sC}" {
+            inherit c sC stdin;
+            inherit (pkg) clustered;
+          };
  in testAll (map check defaultClusters)
