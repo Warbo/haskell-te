6e6449735a8612fc97648d57b5aea38d08593f7e
Chris Warburton <chriswarbo@gmail.com>
2016-08-07 19:45:54 +0100

    Fix test msg

--- tests/pkgTests/haveRawAsts.nix
+++ tests/pkgTests/haveRawAsts.nix
@@ -1,15 +1,17 @@
 defs: with defs; pkg:
 with builtins;
 
-let asts    = pkg.rawDump.stdout;
-    astsNonempty   = testMsg (readFile "${asts}" != "")
-                             "Checking '${asts}' is non-empty";
-    count          = runScript { buildInputs = [ jq ]; } ''
+let asts           = pkg.rawDump.stdout;
+    astsNonempty   = testDbg (readFile "${asts}" != "")
+                             "Checking asts is non-empty"
+                             { inherit asts; };
+    count          = runScript {} ''
                        jq -r 'length' < "${asts}" > "$out"
                      '';
     jCount         = addErrorContext "Parsing: '${count}'" (fromJSON count);
     result         = if count == ""
                         then trace "Got empty count" false
                         else jCount > 0;
-    jCountNonempty = testMsg result "Found no ASTs in '${asts}'";
+    jCountNonempty = testDbg result "Found ASTs in '${asts}'"
+                       { inherit result jCount count asts; };
  in testWrap [ astsNonempty jCountNonempty ] "Have raw ASTs for ${pkg.name}"
