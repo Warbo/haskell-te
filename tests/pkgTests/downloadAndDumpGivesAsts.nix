d5745f1d9e7cd610a5b6414fd36915787f6c36f5
Chris Warburton <chriswarbo@gmail.com>
2016-08-07 17:39:28 +0100

    More info

--- tests/pkgTests/downloadAndDumpGivesAsts.nix
+++ tests/pkgTests/downloadAndDumpGivesAsts.nix
@@ -1,20 +1,28 @@
 defs: with defs; pkg:
 with builtins;
 
-let asts    = quick: downloadAndDump { inherit quick; pkgName = pkg.name; };
-    count   = quick: parseJSON (runScript { buildInputs = [ jq ]; } ''
-                COUNT=$(jq -r 'length' < "${(asts quick).stdout}")
-                if [[ -n "$COUNT" ]]
-                then
-                  echo "$COUNT" > "$out"
-                else
-                  echo '"null"' > "$out"
-                fi
-              '');
-    found   = quick: let result  = count quick;
-                         resultN = fromJSON result;
-                      in testMsg (if resultN == null
-                                     then trace "Got null count" false
-                                     else resultN > 0)
-                                 "Got '${result}' downloaded & dumped ASTs";
- in testAll [(found true) (found false)]
+let count = asts: parseJSON (runScript {} ''
+              COUNT=$(jq -r 'length' < "${asts.stdout}")
+              if [[ -n "$COUNT" ]]
+              then
+                echo "$COUNT" > "$out"
+              else
+                echo '"null"' > "$out"
+              fi
+            '');
+    found = quick: let asts    = downloadAndDump {
+                                   inherit quick;
+                                   pkgName = pkg.name;
+                                 };
+                       result  = count asts;
+                       resultN = fromJSON result;
+                    in testDbg (if resultN == null
+                                   then trace "Got null count" false
+                                   else resultN > 0)
+                               "Got downloaded & dumped ASTs"
+                               { inherit quick result asts;
+                                 inherit (pkg) name };
+ in testRec {
+      fast = found true;
+      slow = found false;
+    }
