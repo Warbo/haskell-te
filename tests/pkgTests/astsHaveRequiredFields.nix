2b693a0bcf504e73dfc3c4c1583ef7c598090ad6
Chris Warburton <chriswarbo@gmail.com>
2016-08-08 13:09:23 +0100

    Split up test

--- tests/pkgTests/astsHaveRequiredFields.nix
+++ tests/pkgTests/astsHaveRequiredFields.nix
@@ -2,32 +2,26 @@ defs: with defs; pkg:
 with builtins;
 with lib;
 
-let result = asts:
-      let str = runScript {
-                  buildInputs = [ tests.pkgTests.haveRawAsts."${pkg.name}" ];
-                } ''
-            RESULTS="{}"
-            for FIELD in package module name ast
-            do
-              # RESULTS accumulates whether all ASTs have each field, and that
-              # they're non-empty
-              RESULTS=$("${jq}/bin/jq" -n --argfile asts    "${asts}"          \
-                                          --argfile results <(echo "$RESULTS") \
-                                          --arg     field   "$FIELD"           \
-                '$results + {($field) : ($asts                             |
-                                         map(has($field) and
-                                             (.[$field] | length | . > 0)) |
-                                         all)}')
-            done
-            echo "$RESULTS" > "$out"
-          '';
-          json = parseJSON str;
-       in assert isString asts;
-          assert isString str;
-          assert isAttrs  json;
-          json;
-    check = asts: testAll (mapAttrsToList (f: b: testMsg b "Checking for ${f}")
-                                          (result asts));
+let result = asts: field:
+      drvFromScript { inherit field; } ''
+        R=$(jq -n --argfile asts    "${asts}" \
+                  --arg     field   "$field"  \
+                  '{($field) : ($asts                             |
+                                map(has($field) and
+                                    (.[$field] | length | . > 0)) |
+                                all)}')
+        [[ "x$R" = "xtrue" ]] || exit 1
+        touch "$out"
+      '';
+
+    check = asts: listToAttrs (map (n: { name = n; value = result asts n; })
+                                   ["package" "module" "name" "ast"]);
+
     slow    = processPackages { quick = false; };
     slowPkg = slow."${pkg.name}";
- in testAll (map check [ pkg.dump pkg.rawDump.stdout slowPkg.dump slowPkg.rawDump.stdout ])
+ in {
+   pkgDump        = check     pkg.dump;
+   pkgRawDump     = check     pkg.rawDump.stdout;
+   slowPkgDump    = check slowPkg.dump;
+   slowPkgRawDump = check slowPkg.rawDump.stdout;
+ }
