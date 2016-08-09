cbccb7ffc26a9707e069c4cc028992651f0b292d
Chris Warburton <chriswarbo@gmail.com>
2016-08-09 14:34:24 +0100

    More debug

--- tests/pkgTests/astsHaveRequiredFields.nix
+++ tests/pkgTests/astsHaveRequiredFields.nix
@@ -3,16 +3,20 @@ with builtins;
 with lib;
 
 let result = asts: field:
-      drvFromScript { inherit asts field; } ''
-        R=$(jq -n --argfile asts    "$asts"   \
-                  --arg     field   "$field"  \
-                  '{($field) : ($asts                             |
-                                map(has($field) and
-                                    (.[$field] | length | . > 0)) |
-                                all)}')
-        [[ "x$R" = "xtrue" ]] || exit 1
-        touch "$out"
-      '';
+      testRun "Have field ${field}"
+              { inherit asts field; }
+              { inherit asts field; }
+              ''
+                R=$(jq -n --argfile asts    "$asts"   \
+                          --arg     field   "$field"  \
+                          '{($field) : ($asts                             |
+                                        map(has($field) and
+                                            (.[$field] | length | . > 0)) |
+                                        all)}')
+                echo "Result: $R" 1>&2
+                [[ "x$R" = "xtrue" ]] || exit 1
+                touch "$out"
+              '';
 
     check = asts: listToAttrs (map (n: { name = n; value = result asts n; })
                                    ["package" "module" "name" "ast"]);
