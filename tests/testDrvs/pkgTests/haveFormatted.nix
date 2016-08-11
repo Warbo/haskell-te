3fd7742ef2a23cf0a741603a399fb1bc293cf361
Chris Warburton <chriswarbo@gmail.com>
2016-08-11 14:27:28 +0100

    Fix haveFormatted

--- tests/testDrvs/pkgTests/haveFormatted.nix
+++ tests/testDrvs/pkgTests/haveFormatted.nix
@@ -2,27 +2,37 @@ defs: with defs; pkg:
 with builtins;
 with lib;
 
-testRun "Have formatted ASTs for ${pkg.name}" null { inherit (pkg) formatted; }
-  ''
+let toAttrs = l: n: if l == []
+                       then {}
+                       else { "${toString n}" = head l; } //
+                            toAttrs (tail l) (n+1);
+
+    doTest = cCount: cs: mapAttrs (go cCount) (toAttrs cs 1);
+
+    go = cCount: c: f: testRun
+           "Have formatted ASTs for ${c}/${cCount} cluster of ${pkg.name}"
+           null
+           { formatted = f; }
+           ''
     function jqMatch {
       RESULT=$(jq -r "$1" < "$formatted")
       [[ "x$RESULT" = "x$2" ]] && return 0
 
       echo "jqMatch for '$1' found '$RESULT', not '$2'" 1>&2
+
+      cat "$formatted" 1>&2
       exit 1
     }
 
-    echo "Checking we got a set" 1>&2
-    jqMatch 'type' 'object'
+    echo "Checking we got an array" 1>&2
+    jqMatch 'type | . == "array"' 'true'
 
-    echo "Checking keys are numeric" 1>&2
-    jqMatch 'keys | map(. as $this | try tonumber catch $this | type | . == "number") | all' 'true'
+    echo "Checking lists contain objects" 1>&2
+    jqMatch 'map(type | . == "object") | all' 'true'
 
-    echo "Checking values are lists" 1>&2
-    jqMatch '[.[]] | map(type | . == "array") | all' 'true'
-
-    echo "Checking lists contain strings" 1>&2
-    jqMatch '[.[]] | add | map(type | . == "string") | all' 'true'
+    echo "Checking objects contain features" 1>&2
+    jqMatch 'map(.features | type | . == "array") | all' 'true'
 
     exit 0
-  ''
+  '';
+in mapAttrs doTest pkg.formatted
