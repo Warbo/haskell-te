5cfc232a83d6f26e0636f8721508189c737a6f30
Chris Warburton <chriswarbo@gmail.com>
2016-08-12 15:20:36 +0100

    Defer test

--- tests/tagging.nix
+++ tests/tagging.nix
@@ -1,11 +1,14 @@
 defs: with defs;
 with builtins;
 
-let result = runScript { buildInputs = [ GetDeps ]; } ''
-        INPUT1='[{"name": "n1", "module": "M1"}, {"name": "n2", "module": "M2"}]'
-        INPUT2='[{"name": "n2", "module": "M2", "foo": "bar"}]'
-        RESULT=$(echo "$INPUT1" | "${tagAstsScript}" <(echo "$INPUT2") "{}")
-        echo "$RESULT" | jq 'type' > "$out"
-      '';
-    jResult = fromJSON result;
- in testMsg (jResult == "array") "Expected 'array', got '${jResult}'"
+testRun "Got array" null { buildInputs = [ GetDeps ]; } ''
+  INPUT1='[{"name": "n1", "module": "M1"}, {"name": "n2", "module": "M2"}]'
+  INPUT2='[{"name": "n2", "module": "M2", "foo": "bar"}]'
+  RESULT=$(echo "$INPUT1" | "${tagAstsScript}" <(echo "$INPUT2") "{}")
+
+  T=$(echo "$RESULT" | jq -r 'type')
+  [[ "x$T" = "xarray" ]] && exit 0
+
+  echo -e "INPUT1: $INPUT1\nINPUT2: $INPUT2\nRESULT: $RESULT\nT: $T" 1>&2
+  exit 1
+''
