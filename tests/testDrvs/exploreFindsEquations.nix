590d30486eb1917627ba1bdc02bc89d1a1de811d
Chris Warburton <chriswarbo@gmail.com>
2016-08-12 14:19:48 +0100

    Keep stderr separate when trying to parse

--- tests/testDrvs/exploreFindsEquations.nix
+++ tests/testDrvs/exploreFindsEquations.nix
@@ -16,29 +16,43 @@ foundEquations = name: f:
         set -x
 
         function finish {
-          echo "OUTPUT: $OUTPUT" 1>&2
+          if [[ -e sout ]]
+          then
+            echo -e "\n\nstdout:\n\n" 1>&2
+            cat sout                  1>&2
+          else
+            echo "No stdout produced" 1>&2
+          fi
+
+          if [[ -e serr ]]
+          then
+            echo -e "\n\nstderr:\n\n" 1>&2
+            cat serr                  1>&2
+          else
+            echo "No stderr produced" 1>&2
+          fi
+
           echo  "COUNT: $COUNT"  1>&2
         }
         trap finish EXIT
 
-
         echo "Exploring '$f'" 1>&2
-        OUTPUT=$("${explore.explore-theories f}" < "$f" 2>&1) || {
-          echo -e "Failed to explore '$f', output follows:\n$OUTPUT\nEND" 1>&2
+        "${explore.explore-theories f}" < "$f" 1> sout 2> serr || {
+          echo -e "Failed to explore '$f'"
           exit 2
         }
 
-        if echo "$OUTPUT" | grep "No clusters found"
+        if grep "No clusters found" < serr
         then
           echo "No clusters found by MLSpec (did it receive any input?)" 1>&2
           exit 3
         fi
 
-        COUNT=$(echo "$OUTPUT" | jq 'length')
+        COUNT=$(jq 'length' < sout)
 
         if [[ "$COUNT" -eq 0 ]]
         then
-          echo -e "Couldn't find any equations in output of '$f':\n$OUTPUT" 1>&2
+          echo -e "Couldn't find any equations in output of '$f'" 1>&2
           exit 0
         else
           echo "Found '$COUNT' equations for '$f'" 1>&2
