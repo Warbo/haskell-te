fd890ecd1710cb6a356b3f2caa0fe95d73fd52f0
Chris Warburton <chriswarbo@gmail.com>
2016-08-15 02:07:40 +0100

    Fixes

--- tests/pkgTests/validFormatting.nix
+++ tests/pkgTests/validFormatting.nix
@@ -2,17 +2,19 @@ defs: with defs; pkg:
 with lib;
 with builtins;
 
-let check = data: drvFromScript { buildInputs = [ jq ]; } ''
+let check = data: drvFromScript { buildInputs = [ jq ]; inherit data; } ''
       set -e
-      LENGTH=$(jq -c 'length' < "${data}") || {
-        echo "Can't read formatted cluster from '${data}' for '${pkg.name}'" 1>&2
+      LENGTH=$(jq -c 'length' < "$data") || {
+        echo "Can't read formatted cluster from '$data' for '${pkg.name}'" 1>&2
         exit 2
       }
       touch "$out"
     '';
 
     checkInner = lst:
-      listToAttrs (map (data: { name  = hashString "sha256" data;
+      listToAttrs (map (data: { name  = hashString "sha256"
+                                          (unsafeDiscardStringContext
+                                            data.outPath);
                                 value = check data; })
                        lst);
 
