408e5012e225ea931bf602bc1e969808d911d564
Chris Warburton <chriswarbo@gmail.com>
2016-08-22 15:21:09 +0100

    Just run existing tests

--- tests/ml4hsfeOuterLoop.nix
+++ tests/ml4hsfeOuterLoop.nix
@@ -1,21 +1,18 @@
 defs: with defs;
 with builtins;
 
-drvFromScript { EXAMPLE = let f = ./ml4hsfeExamples/ml4hsfe-outer-loop-example-input.json;
-                           in drvFromScript {} ''
-                                # Select 50 random ASTs
-                                ASTS=$(jq -c '.[]' < "${f}")
-                                echo "$ASTS" | shuf | head -n50 | \
-                                  jq -s '.' > "$out"
-                              '';
-                buildInputs = [ haskellPackages.ML4HSFE runWeka ]; } ''
-  set -e
-
-  export WIDTH=30
-  export HEIGHT=30
-  export CLUSTERS=4
-
-  ml4hsfe-outer-loop < "$EXAMPLE" | jq '.'
-
-  touch "$out"
-''
+testRun "ML4HSFE test suite" null
+        { ml4hsfe = haskellPackages.ML4HSFE.src;
+          buildInputs = [ (haskellPackages.ghcWithPackages (h: [
+                            h.cabal-install h.ghc h.ML4HSFE h.quickspec h.tasty
+                            h.tasty-quickcheck
+                          ]))
+                          runWeka ]; }
+        ''
+          set -e
+          export HOME="$PWD"
+          cp -r "$ml4hsfe" ./ml4hsfe
+          chmod +w -R ./ml4hsfe
+          cd ./ml4hsfe
+          ./test.sh
+        ''
