df18ccd8ae2ed84ff336f585a5040450eb9c0667
Chris Warburton <chriswarbo@gmail.com>
2016-08-23 17:46:18 +0100

    Extra reduce debug

--- nix-support/reduce.nix
+++ nix-support/reduce.nix
@@ -5,25 +5,42 @@ with lib;
 
 rec {
 
-script = writeScript "reduce-equations" "reduce-equations";
+script = writeScript "reduce-equations" ''
+  INPUT=$(cat)
+  echo "Got input: $INPUT" 1>&2
+  echo "$INPUT" | reduce-equations 1> "$TEMPDIR/reduce-stdout" \
+                                   2> "$TEMPDIR/reduce-stderr"
 
-mkCat = inputs: "cat " + concatStringsSep " " (map (x: ''"${x}"'') inputs);
+  SOUT=$(cat "$TEMPDIR/reduce-stdout")
+  SERR=$(cat "$TEMPDIR/reduce-stderr")
+  echo -e "STDOUT START\n\n$SOUT\n\nSTDOUT END" 1>&2
+  echo -e "STDERR START\n\n$SERR\n\nSTDERR END" 1>&2
+
+  cat "$TEMPDIR/reduce-stdout"
+'';
 
 doReduce = quick: clusterCount: inputs:
-             let allInputs = mkCat inputs;
-              in drvFromScript { inherit inputs;
-                                 outputs = stdParts;
-                                 buildInputs = explore.extractedEnv {
-                                                 extraHs = [ "reduce-equations" ];
-                                                }; } ''
-                  set -e
-                  export CLUSTERS="${clusterCount}"
-                  O=$(cat $inputs | "${benchmark {
-                                         inherit quick inputs;
-                                         cmd = toString script;
-                                     }}")
-                  ${storeParts}
-                '';
+             drvFromScript { inherit inputs;
+                             outputs = stdParts;
+                             buildInputs = explore.extractedEnv {
+                                             extraHs = [ "reduce-equations" ];
+                                           }; } ''
+               set -e
+               export CLUSTERS="${clusterCount}"
+
+               function getEqs {
+                 for F in $inputs
+                 do
+                   jq -c '.[]' < "$F"
+                 done
+               }
+
+               O=$(getEqs | "${benchmark {
+                               inherit quick inputs;
+                               cmd = toString script;
+                             }}")
+               ${storeParts}
+             '';
 
 reduce = { quick, explored }:
   let results = mapAttrs (doReduce quick) explored;
