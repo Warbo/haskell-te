991e858dffe0bea98485bb8c2a1fc4c93f4cac50
Chris Warburton <chriswarbo@gmail.com>
2016-09-09 22:56:22 +0100

    Set unicode env for reduce tests

--- tests/reduce.nix
+++ tests/reduce.nix
@@ -2,7 +2,11 @@ defs: with defs;
 
 testRun "reduce-equations test suite" null
         {
+          LANG           = "en_US.UTF-8";
+          LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+
           d = haskellPackages.reduce-equations.src;
+
           buildInputs = [
             cabal-install
             (haskellPackages.ghcWithPackages (h: [
