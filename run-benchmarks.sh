f00784b4f15c54759b6bd8642a0923b17478200f
Chris Warburton <chriswarbo@gmail.com>
2016-09-15 19:39:46 +0100

    Only benchmark what we don't have

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -11,16 +11,22 @@ for F in $FILES
 do
     NAME=$(basename "$F" .smt2)
 
-    echo "Benchmarking '$F' with mlspec" 1>&2
-
     for N in 1 2 3 4
     do
-        nix-shell -p "(import \"\${$BASE}/release.nix\").package" --run \
-          "DO_BENCH=1 CLUSTERS=$N mlspecBench < '$F' 1> '$NAME-mlspec-$N.stdout' 2> '$NAME-mlspec-$N.stderr'"
+        OUT="$NAME-mlspec-$N.stdout"
+        if ! [[ -e "$OUT" ]]
+        then
+            echo "Benchmarking '$F' with '$N' mlspec clusters" 1>&2
+            nix-shell -p "(import \"\${$BASE}/release.nix\").package" --run \
+              "DO_BENCH=1 CLUSTERS=$N mlspecBench < '$F' 1> '$OUT' 2> '$NAME-mlspec-$N.stderr'"
+        fi
     done
 
-    echo "Benchmarking '$F' with quickspec" 1>&2
-
-    nix-shell -p "(import \"\${$BASE}/release.nix\").package" --run \
-      "DO_BENCH=1 quickspecBench < '$F' 1> '$NAME-quickspec-$N.stdout' 2> '$NAME-quickspec-$N.stderr'"
+    OUT="$NAME-quickspec.stdout"
+    if ! [[ -e "$OUT" ]]
+    then
+        echo "Benchmarking '$F' with quickspec" 1>&2
+        nix-shell -p "(import \"\${$BASE}/release.nix\").package" --run \
+          "DO_BENCH=1 quickspecBench < '$F' 1> '$OUT' 2> '$NAME-quickspec.stderr'"
+    fi
 done
