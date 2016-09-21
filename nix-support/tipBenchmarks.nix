981ae7a27bf6ef61ad5bdd39d57823c226ede2fa
Chris Warburton <chriswarbo@gmail.com>
2016-09-21 16:03:43 +0100

    Cache tip benchmarks more strongly

--- nix-support/tipBenchmarks.nix
+++ nix-support/tipBenchmarks.nix
@@ -1,10 +1,12 @@
 { bash, callPackage, defaultClusters, fetchurl, haskellPackages, nixFromCabal,
-  processPackage, python, racket, runScript, stdenv, writeScript }:
+  processPackage, python, racket, runScript, stdenv, storeResult, writeScript }:
 
 with builtins;
 let path = if any (x: x.prefix == "te-benchmarks") nixPath
               then <te-benchmarks>
-              else ../packages/te-benchmark;
+              else runScript {} ''
+                     "${storeResult}" "${../packages/te-benchmark}"
+                   '';
  in rec {
   inherit (callPackage path {})
     te-benchmark tip-benchmarks te-benchmark-tests tip-benchmark-smtlib;
