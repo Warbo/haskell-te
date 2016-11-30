67136a0aaf9305e1b3dc2d1ed49b14c9107488bc
Chris Warburton <chriswarbo@gmail.com>
2016-11-30 18:42:59 +0000

    Remove redundant test

--- tests/pkgTests/canGetTypes.nix
+++
@@ -1,7 +0,0 @@
-defs: with defs; pkg:
-with builtins;
-
-let count = fromJSON (parseJSON (runScript {} ));
- in testRun "Found types for '${pkg.name}'" null { buildInputs = [ jq ]; } ''
-      jq -re 'map(has("type") and .type != null) | any' < "${pkg.annotated}" > "$out"
-    ''
