03325148de693ba19f38eb4d12f3385e9c33a869
Chris Warburton <chriswarbo@gmail.com>
2017-02-14 18:32:43 +0000

    Add dependency to test

--- tests/quickspecBench.nix
+++ tests/quickspecBench.nix
@@ -34,44 +34,49 @@ with {
   nat-full   = testFile "nat-full"   ../benchmarks/nat-full.smt2;
   nat-simple = testFile "nat-simple" ../benchmarks/nat-simple.smt2;
 
-  sample-tip = runCommand "sample-tip" { buildInputs = [ jq package ]; }''
-    #!/usr/bin/env bash
-    set -e
-    BENCH_OUT=$(SAMPLE_SIZES="5" quickspecBench)
+  sample-tip = runCommand "sample-tip"
+    {
+      buildInputs = [ jq package tipBenchmarks.tools ];
+    }
+    ''
+      #!/usr/bin/env bash
+      set -e
+      BENCH_OUT=$(SAMPLE_SIZES="5" quickspecBench)
 
-    # Get all the constant symbols in all equations
-    STDOUTS=$(echo "$BENCH_OUT" | jq -r '.results | .[] | .stdout') ||
-      ${fail "Couldn't get stdouts\n\n$BENCH_OUT"}
+      # Get all the constant symbols in all equations
+      STDOUTS=$(echo "$BENCH_OUT" | jq -r '.results | .[] | .stdout') ||
+        ${fail "Couldn't get stdouts\n\n$BENCH_OUT"}
 
-    OUTPUTS=$(while read -r F
-              do
-                cat "$F"
-              done < <(echo "$STDOUTS")) ||
-      ${fail "Couldn't concat stdouts\n\n$BENCH_OUT\n\n$STDOUTS"}
+      OUTPUTS=$(while read -r F
+                do
+                  cat "$F"
+                  echo ""
+                done < <(echo "$STDOUTS")) ||
+        ${fail "Couldn't concat stdouts\n\n$BENCH_OUT\n\n$STDOUTS"}
 
-    EQS=$(echo "$OUTPUTS" | grep -v '^Depth') ||
-      ${fail "Couldn't get eqs\n\n$BENCH_OUT\n\n$OUTPUTS"}
+      EQS=$(echo "$OUTPUTS" | grep -v '^Depth') ||
+        ${fail "Couldn't get eqs\n\n$BENCH_OUT\n\n$OUTPUTS"}
 
-    NAMES=$(echo "$EQS" |
-            jq -r 'getpath(paths(type == "object" and .role == "constant")) |
-                   .symbol' |
-            sort -u) || ${fail "Couldn't get names\n\n$BENCH_OUT\n\n$EQS"}
-    SAMPLE=$(choose_sample 5 1)
+      NAMES=$(echo "$EQS" |
+              jq -r 'getpath(paths(type == "object" and .role == "constant")) |
+                     .symbol' |
+              sort -u) || ${fail "Couldn't get names\n\n$BENCH_OUT\n\n$EQS"}
+      SAMPLE=$(choose_sample 5 1)
 
-    # Remove any names which appear in the sample
-    while read -r NAME
-    do
-      NAMES=$(echo "$NAMES" | grep -vFx "$NAME") || true
-    done < <(echo "$SAMPLE")
+      # Remove any names which appear in the sample
+      while read -r NAME
+      do
+        NAMES=$(echo "$NAMES" | grep -vFx "$NAME") || true
+      done < <(echo "$SAMPLE")
 
-    # If there are any names remaining, they weren't in the sample
-    if echo "$NAMES" | grep '^.' > /dev/null
-    then
-      echo "Found names which aren't in sample" 1>&2
-      echo -e "NAMES:\n$NAMES\n\nOUTPUT:\n$BENCH_OUT\nSAMPLE:\n$SAMPLE" 1>&2
-      exit 1
-    fi
+      # If there are any names remaining, they weren't in the sample
+      if echo "$NAMES" | grep '^.' > /dev/null
+      then
+        echo "Found names which aren't in sample" 1>&2
+        echo -e "NAMES:\n$NAMES\n\nOUTPUT:\n$BENCH_OUT\nSAMPLE:\n$SAMPLE" 1>&2
+        exit 1
+      fi
 
-    echo "Passed" > "$out"
-  '';
+      echo "Passed" > "$out"
+    '';
 }
