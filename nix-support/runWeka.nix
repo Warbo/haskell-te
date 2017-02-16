131f78874ab254f3ce57964d337ecd427b172b6e
Chris Warburton <chriswarbo@gmail.com>
2017-02-16 12:24:24 +0000

    Parameterise JVM

--- nix-support/runWeka.nix
+++ nix-support/runWeka.nix
@@ -5,7 +5,7 @@ callPackage (if havePath "runWeka"
                 else fetchFromGitHub {
                        owner  = "Warbo";
                        repo   = "run-weka";
-                       rev    = "a22cd2c";
-                       sha256 = "153k060k3wzqqir4amv0f5viwm8g419abk46hh61s4psb7cascka";
+                       rev    = "6a6f627";
+                       sha256 = "14c199nakf9khwbb02n675dhf8b73y2cxpvjwxijfinkankdad7g";
                      })
             {}
