5a21e6b88d58c12fbb709b7ed645093b61fcde8c
Chris Warburton <chriswarbo@gmail.com>
2017-02-20 11:19:21 +0000

    Check if values are hashable

--- nix-support/runTypesScript.nix
+++ nix-support/runTypesScript.nix
@@ -18,6 +18,7 @@ replLines = writeScript "replLines" ''
               done
             '';
 
+# Run GHCi with all relevant packages available
 repl = let cmd = "nix-shell --run 'ghci -v0 -XTemplateHaskell'";
            msg = "No default.nix found " + toJSON { inherit pkgSrc; };
            given = assert isString pkgSrc || typeOf pkgSrc == "path" ||
@@ -28,13 +29,17 @@ repl = let cmd = "nix-shell --run 'ghci -v0 -XTemplateHaskell'";
                    assert pathExists (unsafeDiscardStringContext "${pkgSrc}/default.nix") ||
                              abort "Couldn't find '${pkgSrc}/default.nix'";
                    ''(x.callPackage "${pkgSrc}" {})'';
-           hsPkgs = "[x.QuickCheck x.quickspec ${given}]";
+           hsPkgs = "[x.QuickCheck x.quickspec x.hashable ${given}]";
         in writeScript "repl" ''
              ${cmd} -p "haskellPackages.ghcWithPackages (x: ${hsPkgs})"
            '';
 
+# Writes GHCi commands to stdout, which we use to test the types of terms
 typeCommand = writeScript "typeCommand" ''
                 echo ":m"
+
+                # Used for hashing values, to reduce memory usage.
+                echo "import Data.Hashable"
 
                 while read -r MOD
                 do
@@ -53,40 +58,98 @@ typeCommand = writeScript "typeCommand" ''
 # Try to type-check QuickSpec signatures, to see which work
 # TODO: Higher-kinded polymorphism, eg. Functors and Monads
 mkQuery = writeScript "mkQuery" ''
+            # The name of the value we're trying to send through QuickSpec
+            GIVEN="$1"
+
             # Shorthand
             QS="Test.QuickSpec"
 
-            # Use Template Haskell to monomorphise our function (tries to
+            # Use Template Haskell to monomorphise our value (tries to
             # instantiate all type variables with "Integer")
             MONO="Test.QuickCheck.All.monomorphic ('$1)"
 
             # We must use a layer of let/in for TH to work, so we call our
-            # monomorphic function "f"
+            # monomorphic value "f"
             BIND="let f = \$($MONO) in"
 
             # Get the monomorphised type
             echo ":t $BIND f"
 
-            # Try turning our monomorphised function into a QuickSpec Sig(nature)
-            for NUM in 5 4 3 2 1 0
-            do
-              # Format the current arity and the (qualified) name as JSON. If this
-              # works, this term is 'quickspecable' (i.e. suitable for use in a
-              # QuickSpec signature)
-              JSON="\"{\\\"arity\\\": $NUM, \\\"qname\\\": \\\"$1\\\", \\\"quickspecable\\\": true}\""
+            # See if our monomorphised value can be added to a QuickSpec
+            # Sig(nature). This can be done in two ways:
+            #
+            #  - Directly, using `fun0`, `fun1`, `fun2`, etc. depending on the
+            #    arity. This requires the type (or result type, for functions)
+            #    be an instance of `Ord`. Values of this type will be compared
+            #    to discover equivalence classes; such values can build up on
+            #    the heap, causing memory exhaustion.
+            #  - Indirectly, by adding our value using one of the `blind0`,
+            #    `blind1`, etc. functions (depending on arity) which don't
+            #    compare (or store) their outputs. We then add a hash function
+            #    to the signature using `observer1`; whenever our function
+            #    generates an output (or any other value of that type is
+            #    produced) they're hashed into an `Int` for storage and
+            #    comparison.
+            #
+            # We prefer the indirect method, to keep down memory usage.
 
-              # Pass f to QuickSpec, using our JSON as the identifier
-              SIG="$QS.fun$NUM ($JSON) f"
+            function tryCall() {
+              # Try to make a QuickSpec signature using the given parameters,
+              # writing JSON to stdout on success.
+              #  - $1 is the function to call, an arbitrarily complex expression
+              #  - $2 is the arity we'll report in our JSON
+              #  - $3 is whether results are hashable (indirect) or not
 
-              # Extract the JSON from the signature
-              EXPR="$QS.Term.name (head ($QS.Signature.symbols ($SIG)))"
+              # Construct the JSON we'll send to stdout. This is double-escaped:
+              #  - We need to use "" in the shell to splice in variables
+              #  - We're generating Haskell code, which uses "" for strings
+              #  - The Haskell string contains JSON, which uses "" for strings
+              # We include the given name, the given arity and whether it's
+              # hashable. We can assume it's quickspecable, since the message
+              # won't appear if it isn't.
+              QNAME="\\\"qname\\\": \\\"$GIVEN\\\""
+              FIELDS="$QNAME, \\\"quickspecable\\\": true"
+              JSON="\"{\\\"arity\\\": $NUM, \\\"hashable\\\":$3, $FIELDS}\""
+
+              # We use the given function to add our term (monomorphised as `f`)
+              # to a QuickSpec signature; we use the above JSON as its name. We
+              # extract this name and print it out; if this works, then the term
+              # must be suitable for use in QuickSpec.
+
+              EXPR="$QS.Term.name (head ($QS.Signature.symbols ($1 ($JSON) f)))"
 
               # Print out the JSON, so we can spot it when we parse the results
               echo "$BIND putStrLn ($EXPR)"
+            }
+
+            # Try `blind` functions first; the higher the arity the better,
+            # since outputting curried functions will likely prevent comparison.
+            for NUM in 5 4 3 2 1 0
+            do
+              # We try calling our value as a function with $NUM arguments, then
+              # send the result through `Data.Hashable.hash`.
+              CALL="f"
+              for ARG in $(seq 1 "$NUM")
+              do
+                CALL="$CALL undefined"
+              done
+              CALL="Data.Hashable.hash ($CALL)"
+
+              # We don't need the result of the hash call, so we put it in an
+              # unused let/in variable; the result we want is a call to `blind`
+              tryCall "let g = $CALL in $QS.blind$NUM" "$NUM" true
+            done
+
+            # If we can't hash, try adding directly (requires output be `Ord`)
+            for NUM in 5 4 3 2 1 0
+            do
+              # Try constructing a signature using `fun5`, `fun4`, etc. until
+              # we either get a success, or run out (not QuickSpecable).
+              tryCall "$QS.fun$NUM" "$NUM" false
           done
         '';
 
-# Make sure the types we've been given are actually available in scope (ie.
+# Makes sure the types we've been given are actually available in scope (ie.
 # they're not off in some hidden package)
 typeScopes = writeScript "typeScopes" ''
                echo ":m"
@@ -105,6 +168,7 @@ typeScopes = writeScript "typeScopes" ''
                done
              '';
 
+# Makes sure that the modules we've been given can be imported.
 checkMods = writeScript "checkMods" ''
   IMPORTS=$(echo "$MODS" |
             while read -r MOD
