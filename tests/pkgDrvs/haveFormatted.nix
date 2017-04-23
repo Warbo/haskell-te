c4a9d230002b40b6a7320cae9c3b1f02b0f643d9
Chris Warburton <chriswarbo@gmail.com>
2017-04-23 08:51:21 +0100

    Don't check for features in formatted result
    It was making our JSON really big, and unwieldy

--- tests/pkgDrvs/haveFormatted.nix
+++ tests/pkgDrvs/haveFormatted.nix
@@ -15,23 +15,16 @@ let toAttrs = l: n: if l == []
            { formatted = f; }
            ''
     function jqMatch {
-      RESULT=$(jq -r "$1" < "$formatted")
-      [[ "x$RESULT" = "x$2" ]] && return 0
+      jq -e -r "$1" < "$formatted" && return 0
 
-      echo "jqMatch for '$1' found '$RESULT', not '$2'" 1>&2
+      echo "jqMatch for '$1' failed" 1>&2
 
       cat "$formatted" 1>&2
       exit 1
     }
 
-    echo "Checking we got an array" 1>&2
-    jqMatch 'type | . == "array"' 'true'
-
-    echo "Checking lists contain objects" 1>&2
-    jqMatch 'map(type | . == "object") | all' 'true'
-
-    echo "Checking objects contain features" 1>&2
-    jqMatch 'map(.features | type | . == "array") | all' 'true'
+    jqMatch     'type | . == "array"'
+    jqMatch 'map(type | . == "object") | all'
 
     exit 0
   '';
