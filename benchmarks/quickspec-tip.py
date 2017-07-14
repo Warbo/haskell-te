daf300b181b270566e95274e3ed04b25aa16891a
Chris Warburton <chriswarbo@gmail.com>
2017-07-14 16:02:50 +0100

    Make benchmarks more specific

--- benchmarks/quickspec-tip.py
+++ benchmarks/quickspec-tip.py
@@ -1,17 +1,14 @@
 # See "Writing benchmarks" in the asv docs for more information.
 
-from json  import dumps, loads
-from os    import getenv
-from sys   import stderr
-from util  import cached, eqs_in, pipe, sort, timed_run
-
-# Used by our own code
-timeout_secs = 200
+from json       import dumps, loads
+from os         import getenv
+from parameters import repetitions, timeout_secs
+from util       import cached, eqs_in, generate_cache, pipe, sort, timed_run
 
 # Benchmark parameters. Will appear in alphabetical order as arguments, after
 # 'cache'
 params = {
-    'rep'     : range(0, 1),
+    'rep'     : range(0, repetitions),
     'size'    : range(1, 5),
 }
 
@@ -30,37 +27,32 @@ def setup_cache():
     By returning all of the results from setup_cache, each benchmark can pick
     out the values it cares about, without having to re-run anything.
     The returned value will appear as the first argument to each benchmark.'''
-    cache = {}
-    for size in params['size']:
-        cache[size] = [{} for _ in params['rep']]
-        for rep in params['rep']:
-            data = {'timeout': timeout_secs}
+    def gen(size, rep):
+        # Choose a sample, and generate QuickSpec code for exploring it
+        sample, _      = pipe(['choose_sample', str(size), str(rep)])
+        data['sample'] = sample
 
-            # Choose a sample, and generate QuickSpec code for exploring it
-            sample, _      = pipe(['choose_sample', str(size), str(rep)])
-            date['sample'] = sample
+        stdout, _ = pipe([getenv('qsTipSetup')], sample)
+        program   = loads(stdout)
 
-            stdout, _ = pipe([getenv('qsSetup')], sample)
-            program   = loads(stdout)
+        # Run QuickSpec on this sample
+        data.update(timed_run([getenv('qsTipRunner'), program['runner']],
+                              program['code'],
+                              timeout_secs))
 
-            # Run QuickSpec on this sample
-            data.update(timed_run([getenv('qsRunner'), program['runner']],
-                                  program['code'],
-                                  timeout_secs))
+        # Analyse the result, if we have one
+        results = 'null'
+        if data['success']:
+            # conjectures_for_sample expects encoded sample but decoded eqs
+            encoded    = eqs_in(cache[size][rep]['stdout'])
+            decoded, _ = pipe(['decode'], dumps(encoded))
+            results, _ = pipe(['conjectures_for_sample'], decoded,
+                              env={'SAMPLED_NAMES' : sample})
 
-            # Analyse the result, if we have one
-            results = None
-            if data['success']:
-                # conjectures_for_sample expects encoded sample but decoded eqs
-                encoded    = eqs_in(cache[size][rep]['stdout'])
-                decoded, _ = pipe(['decode'], dumps(encoded))
-                results, _ = pipe(['conjectures_for_sample'], decoded,
-                                  env={'SAMPLED_NAMES' : sample})
+        data['conjectures'] = loads(results)
+        return data
 
-            data['conjectures'] = loads(results)
-            cache[size][rep]    = data
-
-    return cache
+    return generate_cache(params['size'], gen)
 setup_cache.timeout = timeout_secs * len(params['rep']) * len(params['size'])
 
 def track_data(cache):
