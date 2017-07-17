4f2ec5963f8cce3b7ce332af88afc5355a54c9d2
Chris Warburton <chriswarbo@gmail.com>
2017-07-17 08:10:59 +0100

    Fix benchmarking environment

--- benchmarks/default.nix
+++ benchmarks/default.nix
@@ -1,6 +1,7 @@
 # Builds the environment in which to run a benchmark
 args:
 
+with builtins;
 with import ../nix-support {};
 with rec {
   inherit (nix-config) attrsToDirs wrap;
@@ -12,20 +13,26 @@ attrsToDirs {
     python = wrap {
       paths = [ tipBenchmarks.tools ];
       vars  = {
+        NIX_EVAL_HASKELL_PKGS = quickspecBench.customHs;
+        NIX_PATH              =
+          "nixpkgs=${toString ./..}/nix-support:real=${toString <nixpkgs>}";
+
         qsTipSetup  = qs.sampled.genInput;
         qsTipRunner = qs.sampled.runner;
 
         qsStandaloneMkPkg  = qs.standalone.genAnnotatedPkg;
         qsStandaloneSetup  = qs.standalone.genInput;
         qsStandaloneRunner = qs.standalone.runner;
-
-        theoryFiles = builtins.toJSON {
+      };
+      script = ''
+        #!/usr/bin/env bash
+        export theoryFiles='${trace "TODO: Fix quoting in wrap" toJSON {
           list-full  = ./list-full.smt2;
           nat-full   = ./nat-full.smt2;
           nat-simple = ./nat-simple.smt2;
-        };
-      };
-      file = "${python}/bin/python";
+        }}'
+        exec "${python}/bin/python" "$@"
+      '';
     };
   };
 }
