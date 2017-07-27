0086d2351db991933dc787ef9aa94d16f528a609
Chris Warburton <chriswarbo@gmail.com>
2017-07-27 12:03:33 +0100

    Check that we have Arbitrary instances for generated datatypes

--- tests/haveExpectedVariables.nix
+++ tests/haveExpectedVariables.nix
@@ -1,29 +1,85 @@
 defs: with defs;
 
 with rec {
-  env = runCommand "have-expected-vars"
+  env = runCommand "theory-env"
     (withNix {
       inherit (quickspecBench.benchVars.standalone)
         genAnnotatedPkg genInput;
-      data = ../benchmarks/list-full.smt2;
+      data        = ../benchmarks/list-full.smt2;
+      buildInputs = [ jq ];
     })
     ''
-      # Make a dir to hold our generated Haskell package; it must be accessible to
-      # other Nix builders, since we'll be invoking Nix recursively.
-      export OUT_DIR="$PWD/pkg"
-      mkdir "$OUT_DIR"
-      chmod 777 -R "$PWD"
-
       # Convert TIP format to Haskell, extract ASTs and annotate with type info
-      ASTS=$("$genAnnotatedPkg" < "$data")
+      RESULT=$("$genAnnotatedPkg" < "$data")
+
+         ASTS=$(echo "$RESULT" | jq -r '.annotated')
+      OUT_DIR=$(echo "$RESULT" | jq -r '.out_dir')
+
+      export OUT_DIR
 
       # Generate a QuickSpec signature for the generated Haskell package, along
       # with the code to explore it.
-      "$genInput" < "$ASTS" > "$out"
+      "$genInput" < "$ASTS" |
+        jq --arg dir "$OUT_DIR" '. + {"out_dir":$dir}' > "$out"
     '';
 
   code = runCommand "code.hs" { inherit env; buildInputs = [ jq ]; } ''
     jq -r '.code' < "$env" > "$out"
+  '';
+
+  # The 'runhaskell' command used to execute the sig generator, run inside the
+  # appropriate Nix environment (for GHC, dependencies, generated package, etc.)
+  runhaskell = nix-config.wrap {
+    vars   = {
+      inherit env;
+      NIX_EVAL_HASKELL_PKGS = quickspecBench.customHs;
+      NIX_PATH              = quickspecBench.innerNixPath;
+      NIX_REMOTE            = "daemon";
+    };
+    paths  = [ jq nix ];
+    script = ''
+      #!/usr/bin/env bash
+       RUNNER=$(jq -r '.runner'  < "$env")
+         EXPR=$(jq -r '.env'     < "$env")
+      OUT_DIR=$(jq -r '.out_dir' < "$env")
+
+      export OUT_DIR
+
+      nix-shell --show-trace -p "$EXPR" --run "$RUNNER"
+    '';
+  };
+
+  countVars = writeScript "countVars.hs" ''
+    -- TODO: We don't need all of these
+    import Test.QuickSpec
+    import Test.QuickSpec.Signature
+    import Data.Digest.Murmur32
+    import Data.Serialize
+    import MLSpec.Helper
+    import A
+    import IfCxt
+    import Test.QuickCheck
+    import Test.RuntimeArbitrary
+    import Prelude
+    import GHC.Types
+
+    mkIfCxtInstances '''Ord
+    mkIfCxtInstances '''Arbitrary
+
+    intLen  = Prelude.length (Test.RuntimeArbitrary.getArbGen [
+        Prelude.undefined :: (Prelude.Integer)
+      ])
+
+    listLen = Prelude.length (Test.RuntimeArbitrary.getArbGen [
+        Prelude.undefined :: ((A.List) Prelude.Integer)
+      ])
+
+    message = case (intLen, listLen) of
+      (0, _) -> error "No Arbitrary instance for Integer"
+      (_, 0) -> error "No Arbitrary instance for (List Integer)"
+      _      -> "Found Arbitrary instances"
+
+    main = putStrLn message
   '';
 };
 {
@@ -52,5 +108,15 @@ with rec {
       done
 
       echo "pass" > "$out"
+    '';
+
+  haveGenerators = runCommand "have-generators"
+    {
+      inherit countVars runhaskell;
+    }
+    ''
+      echo "Checking that we can find Arbitrary instances" 1>&2
+      "$runhaskell" < "$countVars"
+      echo "pass"   > "$out"
     '';
 }
