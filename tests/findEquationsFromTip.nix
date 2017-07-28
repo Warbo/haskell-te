64ee7adc6516b9982d24e56ff70ece878de403e3
Chris Warburton <chriswarbo@gmail.com>
2017-07-28 10:03:06 +0100

    Store code and equations for test, so we can inspect them on failure

--- tests/findEquationsFromTip.nix
+++ tests/findEquationsFromTip.nix
@@ -77,32 +77,48 @@ with rec {
     (withNix {
       buildInputs  = [ jq tipBenchmarks.tools ];
 
-      inherit (quickspecBench.benchVars.standalone)
-        genAnnotatedPkg genInput runner;
-
       NIX_EVAL_HASKELL_PKGS = quickspecBench.customHs;
       GROUND_TRUTH          = ../benchmarks/ground-truth/list-full.smt2;
       TRUTH_SOURCE          = ../benchmarks/ground-truth/list-full.smt2;
-      theory                = ../benchmarks/list-full.smt2;
+
+      SETUP = runCommand "setup.json"
+        (withNix {
+          inherit (quickspecBench.benchVars.standalone)
+            genAnnotatedPkg genInput;
+
+          buildInputs = [ jq ];
+          theory      = ../benchmarks/list-full.smt2;
+
+          NIX_EVAL_HASKELL_PKGS = quickspecBench.customHs;
+        })
+        ''
+          set -e
+
+          DATA=$("$genAnnotatedPkg" < "$theory")
+
+          ANNOTATED=$(echo "$DATA" | jq -r '.annotated')
+            OUT_DIR=$(echo "$DATA" | jq -r '.out_dir'  )
+
+          export OUT_DIR
+
+          "$genInput" < "$ANNOTATED" |
+            jq --arg dir "$OUT_DIR" '. + {"out_dir": $dir}' > "$out"
+        '';
     })
     ''
       set -e
 
-      DATA=$("$genAnnotatedPkg" < "$theory")
-
-      ANNOTATED=$(echo "$DATA" | jq -r '.annotated')
-        OUT_DIR=$(echo "$DATA" | jq -r '.out_dir'  )
+       RUNNER=$(jq -r '.runner'  < "$SETUP")
+         CODE=$(jq -r '.code'    < "$SETUP")
+          ENV=$(jq -r '.env'     < "$SETUP")
+      OUT_DIR=$(jq -r '.out_dir' < "$SETUP")
 
       export OUT_DIR
 
-      SETUP=$("$genInput" < "$ANNOTATED")
-
-      RUNNER=$(echo "$SETUP" | jq -r '.runner')
-        CODE=$(echo "$SETUP" | jq -r '.code'  )
-         ENV=$(echo "$SETUP" | jq -r '.env'   )
-
       EQS=$(echo "$CODE" | nix-shell -p "$ENV" --run "$RUNNER" |
             grep -v '^Depth')
+
+      echo "$EQS" 1>&2
 
       RESULT=$(echo "$EQS" | jq -s '.' | precision_recall_eqs)
 
