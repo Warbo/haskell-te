b2362b73c85434e78d8a287ad84fee973c5c3fcf
Chris Warburton <chriswarbo@gmail.com>
2017-08-10 15:56:28 +0100

    Use fixed version of asv-nix
    We were taking asv-nix from nix-config, but that uses latestGit to check for
    updates, which we don't really want to do in a stable, reproducible environment
    like haskell-te (as much as possible, anyway!)
    
    Now we take asv-nix from a fixed git commit, and pass in its asv dependency from
    nix-config. That's OK, since the (fixed version of) nix-config we're using uses
    a fixed version of asv.

--- nix-support/asv-nix.nix
+++ nix-support/asv-nix.nix
@@ -1,2 +1,13 @@
-{ nix-config }:
-nix-config.asv-nix
+{ callPackage, fetchFromGitHub, nix-config }:
+
+with {
+  src = fetchFromGitHub {
+    owner  = "Warbo";
+    repo   = "asv-nix";
+    rev    = "d5af74d";
+    sha256 = "1jp5a8p5dzh2vb2s9k2wf3j2l9fcm7l47ydqy8wlrjiyqlc4jw7a";
+  };
+};
+callPackage "${src}" {
+  inherit (nix-config) asv;
+}
