9ca01a945a8dc648065c6d643d3044f8e96d14b4
Chris Warburton <chriswarbo@gmail.com>
2017-08-13 16:53:06 +0100

    Bump nix-config to get wrap fixes

--- nix-support/nix-config.nix
+++ nix-support/nix-config.nix
@@ -6,16 +6,16 @@ with rec {
     inherit rev;
     owner  = "Warbo";
     repo   = "nix-config";
-    sha256 = "1kkjdn0bhc9wii3hmi27v0snzkckr44pcg7k2n1p32bg3mmgfi6x";
+    sha256 = "0wn0vjzz975fkrgl7g3h26pjcm5ja2ylzb5d09kf1apkivdxmvfi";
   };
 
   local = fetchgit {
     inherit rev;
     url    = "${getEnv "GIT_REPO_DIR"}/nix-config.git";
-    sha256 = "1zx5bmqxzibz0jcx46ghscl7vx3imvpwjp3lwv9bkcfkxfqc03q6";
+    sha256 = "0isnxvv11lddhdh43pz08rh0an84jfpcdgc9rlbb8hk4dqknkhr1";
   };
 
-  rev    = "cd88c9a";
+  rev    = "bf94098";
   chosen = if getEnv "GIT_REPO_DIR" == ""
               then gh
               else local;
