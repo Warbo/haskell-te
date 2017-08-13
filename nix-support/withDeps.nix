1ab52dde1877f3da9c61260408c8f1da19ad7ca1
Chris Warburton <chriswarbo@gmail.com>
2017-08-13 18:51:32 +0100

    withDeps is just a special case of wrap

--- nix-support/withDeps.nix
+++ nix-support/withDeps.nix
@@ -1,18 +1,15 @@
-{ bash, lib, makeWrapper, runCommand, writeScript }:
+{ bash, lib, makeWrapper, runCommand, wrap, writeScript }:
 with lib;
 with builtins;
 
 name: deps: text:
 
 assert isString name;
-assert isList deps;
+assert isList   deps;
 assert isString text;
 
-with {
-  prefices = fold (dep: rest: rest + '' --prefix PATH : "${dep}/bin"'') "" deps;
-};
-
-runCommand "${name}-with-deps" { buildInputs = [ makeWrapper ]; } ''
-  #!${bash}/bin/bash
-  makeWrapper "${writeScript "${name}-script" text}" "$out" ${prefices}
-''
+wrap {
+  name   = "${name}-with-deps";
+  paths  = deps;
+  script = text;
+}
