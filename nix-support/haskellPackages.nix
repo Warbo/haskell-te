ff0be0fa253e03b28dbf04439a7f0f781f8fac8f
Chris Warburton <chriswarbo@gmail.com>
2017-10-03 16:57:45 +0100

    Simplify selection of Haskell packages by stable/unstable/path
    If a package *can* be given by a path, like <my-pkg>, then we check to see if
    that path has been given or not. If so, we use it. Otherwise, we use latestGit
    from nix-config to select between unstable (latest) or stable (hard-coded)
    versions.

--- nix-support/haskellPackages.nix
+++ nix-support/haskellPackages.nix
@@ -5,125 +5,124 @@ with builtins;
 with lib;
 
 with rec {
-  ghcVersion = superHaskellPackages.ghc.version;
-  reqVersion = "7.10.3";
-
   # Get particular Hackage revisions, when those in haskellPackages don't work
   overrides = callPackage: mapAttrs (n: v: callPackage (callHackage n v) {}) {
-    tasty = "0.11.2.1";
+    haskell-src-exts = "1.19.0";
+    pretty-show      = "1.6.12";
+    tasty            = "0.11.2.1";
   };
 
   # Get packages from git repos
-  hsPkgs = { cabalPath, cabalCheck, callPackage }: overrides callPackage // {
-    AstPlugin = cabalCheck <ast-plugin> (fetchFromGitHub {
-      owner  = "Warbo";
-      repo   = "ast-plugin";
-      rev    = "a04f6fef18bdd6d23d534ea4dd7c7b5b9084ad1c";
+  hsPkgs = { get }: {
+    AstPlugin = get {
+      path   = <ast-plugin>;
+      url    = https://github.com/Warbo/ast-plugin.git;
+      rev    = "a04f6fe";
       sha256 = "1gmkv4l38vpvhg2h8dwv4gf8dq1d0lr0zxd5j9szi90xb8nl2241";
-    }) {};
+    } {};
 
-    bench  = cabalPath (extractTarball (fetchurl {
-      url    = https://github.com/Gabriel439/bench/archive/1.0.1.tar.gz;
-      sha256 = "1amfq2jhwxzy34gyqyvanc46admwlfqs9dk3d7c10aivbl7v1kyb";
-    })) {};
+    bench  = get {
+      url    = https://github.com/Gabriel439/bench.git;
+      rev    = "2575ff3";
+      sha256 = "18xplqhhqn9y7byib01sv75i4wbgg73rz53bn1ppd5mfxhdp8g8x";
+    } {};
 
-    GetDeps = cabalCheck <get-deps> (fetchFromGitHub {
-      owner  = "Warbo";
-      repo   = "getDeps";
-      rev    = "7c02fbc9b0076f0327d95c3aa05cb607a2f3cf73";
-      sha256 = "19g1lyaplclnlyh7y3li08937bqgk58dsblz12hd290crmg999f0";
-    }) {};
+    GetDeps = get {
+      path   = <get-deps>;
+      url    = https://github.com/Warbo/getDeps.git;
+      rev    = "7c02fbc";
+      sha256 = "0iw9ajzmq439qipm5r21dby32xlmdxg8bnsm6bcgm8dr9whhalfl";
+    } {};
 
-    HS2AST = cabalCheck <hs2ast> (fetchFromGitHub {
-      owner  = "Warbo";
-      repo   = "hs2ast";
-      rev    = "469d99977a78484159a7f5f77f8fbeeeea2b33a4";
-      sha256 = "1x2f12s6caj0gaymaw62bmm62ydim78wm2pn18j18fa2l3p7vqyi";
-    }) {};
+    HS2AST = get {
+      path   = <hs2ast>;
+      url    = https://github.com/Warbo/hs2ast.git;
+      rev    = "469d999";
+      sha256 = "1yancv9pd8rnkpla462czsi4kgd8nvndjixmr1s6kkc7xk68zky8";
+    } {};
 
-    ifcxt = cabalCheck <ifcxt> (fetchFromGitHub {
-      owner  = "mikeizbicki";
-      repo   = "ifcxt";
-      rev    = "7f9f876807f33f8fc84d0face54171ebcca57a4a";
-      sha256 = "0mzd5h45rkvj81pdi60p68r0j3lc4h9m4z3b4v8m6xacp9sxiic1";
-    }) {};
+    ifcxt = get {
+      path   = <ifcxt>;
+      url    = https://github.com/mikeizbicki/ifcxt.git;
+      rev    = "7f9f876";
+      sha256 = "1k267zs3w999xg90ddy02l4cpjn14x07cdgqbca3w0ncili2p7a2";
+    } {};
 
-    ML4HSFE = cabalCheck <ml4hsfe> (fetchFromGitHub {
-      owner  = "Warbo";
-      repo   = "ml4hsfe";
-      rev    = "bcdd93b64ae5503d93f1e56ce9cba44004f2ddaa";
-      sha256 = "1ca59xp5mq2bv4kbml32k4xgql03bqi6b4s7pvzdah9fsi76ap6q";
-    }) {};
+    ML4HSFE = get {
+      path   = <ml4hsfe>;
+      url    = https://github.com/Warbo/ml4hsfe.git;
+      rev    = "bcdd93b";
+      sha256 = "0rraj7ias334g39vyc5z9afms4w30g5vf1czgw7fskahiir2vmi1";
+    } {};
 
-    mlspec = cabalCheck <mlspec> (fetchFromGitHub {
-      owner  = "Warbo";
-      repo   = "mlspec";
+    mlspec = get {
+      path   = <mlspec>;
+      url    = https://github.com/Warbo/mlspec.git;
       rev    = "a5f0dd2";
-      sha256 = "0x17y5fb1r77bjzdxq7r8d5b7n5ksljmivcllgisvc1wafjwlrnq";
-    }) {};
+      sha256 = "1m2vs51wqdqlv67z1qcv3jhwapb9dp5k336vjvjkqpr8svg15yas";
+    } {};
 
-    mlspec-helper = cabalCheck <mlspec-helper> (fetchFromGitHub {
-      owner  = "Warbo";
-      repo   = "mlspec-helper";
-      rev    = "1bf9c32e3ec0e519237a0af297d6512907e95959";
-      sha256 = "1g8jwbfdqa84xdh6gp8ica4v0l51jki880fwmmhs3fcl4vz6i4ax";
-    }) {};
+    mlspec-helper = get {
+      path   = <mlspec-helper>;
+      url    = https://github.com/Warbo/mlspec-helper.git;
+      rev    = "1bf9c32";
+      sha256 = "119kb41gv9v2fs1wgvjpr4skz5bbmp2apgllmxbxw4zw3xww1rlr";
+    } {};
 
-    nix-eval = cabalCheck <nix-eval> (fetchFromGitHub {
-      owner  = "Warbo";
-      repo   = "nix-eval";
+    nix-eval = get {
+      path   = <nix-eval>;
+      url    = https://github.com/Warbo/nix-eval.git;
       rev    = "53d18a2";
-      sha256 = "0rplpygiqn6f9aqi09dr67xidb1bks9xnxxrpzi3bq67bdvjzvh1";
-    }) {};
+      sha256 = "1agw1zppbhgk6aaqd75mmvbd6r8d67ap4y5rli1sm1j1q0a5qqx5";
+    } {};
 
-    reduce-equations = cabalCheck <reduce-equations>
-      (fetchFromGitHub {
-        owner  = "Warbo";
-        repo   = "reduce-equations";
-        rev    = "a86199b68e5a3513cc3cf0e579d67ea6cfa311ae";
-        sha256 = "1ncy2carn18fcwpfdfch99b90mwq52a7dal8rn5kv1wk3951w5rg";
-      }) {
-        haskell-src-exts = callPackage (callHackage "haskell-src-exts"
-                                                         "1.19.0") {
-          pretty-show = callPackage (callHackage "pretty-show" "1.6.12") {
-          };
-        };
-    };
+    reduce-equations = get {
+      path   = <reduce-equations>;
+      url    = https://github.com/Warbo/reduce-equations.git;
+      rev    = "a86199b";
+      sha256 = "11dafq0zl6vj1lzybsfpryvc2rqbxxkjjy3v4ab1vb8nzhxfgna3";
+    } {};
 
-    runtime-arbitrary = cabalCheck <runtime-arbitrary>
-      (fetchFromGitHub {
-        owner  = "Warbo";
-        repo   = "runtime-arbitrary";
-        rev    = "5b7ff2f";
-        sha256 = "11gnfmz48vxvf42xs9255r51vbv1sjghvzi60gcrpx3jk38d2gyb";
-      }) {};
+    runtime-arbitrary = get {
+      path   = <runtime-arbitrary>;
+      url    = https://github.com/Warbo/runtime-arbitrary;
+      rev    = "5b7ff2f";
+      sha256 = "11gnfmz48vxvf42xs9255r51vbv1sjghvzi60gcrpx3jk38d2gyb";
+    } {};
 
-    spoon = cabalPath (fetchFromGitHub {
-      owner  = "Warbo";
-      repo   = "spoon";
+    spoon = get {
+      url    = https://github.com/Warbo/spoon.git;
       rev    = "4424f9a";
-      sha256 = "14mn6ygr0wqy4css8wrbxd6b4qvp951xgc206x79fjfva3q6n12g";
-    }) {};
+      sha256 = "1d14qawzf7pimkjccj3f2vy9nc4fb2w00ib8li4kb180fb6wjvq4";
+    } {};
 
-    weigh = cabalPath (fetchgit {
+    weigh = get {
       url    = https://github.com/fpco/weigh.git;
       rev    = "26f8e3e";
       sha256 = "0pmkzlcjfqi41qmrgjyw1y7naclq86kb6mp0i4ni3d1lkiylb9gc";
-    }) {};
+    } {};
   };
 };
 
-assert ghcVersion == reqVersion ||
-       abort "Using GHC ${ghcVersion} (should be ${reqVersion})";
-
+assert let ghcVersion = superHaskellPackages.ghc.version;
+           reqVersion = "7.10.3";
+        in ghcVersion == reqVersion ||
+           abort "Using GHC ${ghcVersion} (should be ${reqVersion})";
 rec {
-  hsOverride = self: super: (hsPkgs rec {
-    inherit (self) callPackage;
-    cabalPath  = p: self.callPackage (nixFromCabal (toString p) null);
-    cabalCheck = path: fallback: cabalPath (if stable
-                                               then fallback
-                                               else path);
-  });
+  hsOverride = self: super: overrides self.callPackage // hsPkgs {
+    get = { path ? null, url, rev, sha256}:
+          with rec {
+            git = nix-config.latestGit {
+              inherit url;
+              stable = { inherit rev sha256; };
+            };
+            src = with tryEval path;
+                  if success && value != null
+                     then value
+                     else git;
+          };
+          self.callPackage (nixFromCabal (toString src) null);
+  };
 
   haskellPackages = superHaskellPackages.override { overrides = hsOverride; };
 }
