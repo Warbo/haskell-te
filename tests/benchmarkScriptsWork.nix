3555567d9383fe967062a2d83037e1171d836742
Chris Warburton <chriswarbo@gmail.com>
2017-10-04 01:18:19 +0100

    Remove obsolete test

--- tests/benchmarkScriptsWork.nix
+++ tests/benchmarkScriptsWork.nix
@@ -12,26 +12,14 @@ let result = script: input: drvFromScript {
                        echo "$input" | "$scriptPath" > "$out"
                      '';
 
-    runResult = result (runCmd  { cmd   = "echo";
-                                   args  = ["hello" "world"]; })
+    runResult = result (runCmd { cmd   = "echo";
+                                 args  = ["hello" "world"]; })
                         "";
 
     runCat = result (runCmd { cmd = "cat"; })
                     "hello world";
 
     checkCat = found: testFieldFile found "hello world";
-
-    testField = found: field: expect:
-      testRun "Checking field ${field}" { inherit field expect; }
-              { inherit expect field found; }
-              ''
-                echo "FOUND: $found" 1>&2
-                GOT=$(jq -cr ".$field" < "$found")
-
-                echo -e "GOT: $GOT\nEXPECT: $expect" 1>&2
-
-                [[ "x$GOT" = "x$expect" ]] || exit 1
-              '';
 
     testFieldFile = found: expect:
       testRun "Checking script output" null
@@ -48,49 +36,7 @@ let result = script: input: drvFromScript {
     check = found: {
       stdout = testFieldFile found "hello world";
     };
-
-    checkInput = args:
-      let shouldFail = testRun "'runCmd' should abort when packages missing"
-                               { inherit allArgs; }
-                               { inherit dbg;
-                                 buildInputs = explore.extractedEnv {}; }
-                               ''
-                                 if "${runCmd allArgs}" < "${inputPkgs}"
-                                 then
-                                   echo "'runCmd' didn't spot error" 1>&2
-                                   echo "$dbg"                          1>&2
-                                   exit 1
-                                 fi
-                               '';
-          shouldSucceed = testRun "'runCmd' works when packages found"
-                                  { inherit allArgs; }
-                                  {
-                                    inherit dbg;
-                                    buildInputs = explore.extractedEnv {
-                                      extraHs = [ "list-extras" "hlint" ];
-                                    };
-                                  }
-                                  ''
-                                    "${runCmd allArgs}" < "${inputPkgs}" || {
-                                      echo "RunCmd didn't work" 1>&2
-                                      echo "$dbg"                  1>&2
-                                      exit 1
-                                    }
-                                  '';
-          # Haskell packages which don't appear in our scripts' dependencies
-          inputPkgs = writeScript "inputs" ''
-                        list-extras
-                        hlint
-                      '';
-          allArgs = args // {
-            cmd    = "true";
-            inputs = [ inputPkgs ];
-          };
-          dbg     = toJSON { inherit allArgs; };
-       in { inherit shouldFail shouldSucceed; };
-
  in {
       echo  = check runResult;
-      input = checkInput {};
       cat   = checkCat runCat;
     }
