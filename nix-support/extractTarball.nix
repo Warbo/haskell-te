ef5486a540b72fbe4117c36cb8acd5ee43b6b54c
Chris Warburton <chriswarbo@gmail.com>
2017-10-04 18:00:00 +0100

    Remove over-engineering from extractTarball

--- nix-support/extractTarball.nix
+++ nix-support/extractTarball.nix
@@ -1,26 +1,28 @@
-{ bash, drvFromScript, gnutar }:
-with builtins;
-tarball:
+{ bash, gnutar, runCommand }:
 
-with {
-  extracted = drvFromScript { buildInputs = [ gnutar ]; } ''
-    #!${bash}/bin/bash
+tarball: runCommand "extract-tarball"
+  {
+    inherit tarball;
+    buildInputs = [ bash gnutar ];
+  }
+  ''
+    #!/usr/bin/env bash
     set -e
 
     # Checking existence in the script avoids 'cannot refer to other
     # paths' errors with pathExists
-    [[ -e "${tarball}" ]] || exit 2
-    tar xf "${tarball}"
-    for D in *
-    do
-      if [[ -d "$D" ]]
-      then
-        nix-store --add "$D" > "$out"
-        exit 0
-      fi
-    done
+    [[ -e "$tarball" ]] || exit 2
+    mkdir extracted
+    pushd extracted
+      tar xf "$tarball"
+      for D in *
+      do
+        if [[ -d "$D" ]]
+        then
+          mv "$D" "$out"
+          exit 0
+        fi
+      done
+    popd
     exit 3
-  '';
-};
-
-import extracted
+  ''
