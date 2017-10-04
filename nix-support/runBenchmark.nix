b2f4ab831d1657b0ca118d6e0565c17e509d86aa
Chris Warburton <chriswarbo@gmail.com>
2017-10-04 14:37:00 +0100

    Indentation

--- nix-support/runBenchmark.nix
+++ nix-support/runBenchmark.nix
@@ -61,92 +61,91 @@ rec {
   };
 
   # Check that the required Haskell packages are found in the environment
-  checkHsEnv = extra: wrap {
-          name   = "checkHsEnv";
-          vars   = {
-            allGiven = concatStringsSep "\n"
-                         (extra ++ explore.extra-haskell-packages);
+  checkHsEnv = more: wrap {
+    name   = "checkHsEnv";
+    vars   = {
+      allGiven = concatStringsSep "\n" (more ++ explore.extra-haskell-packages);
 
-            findHsPkgReferences =
-              let extractionScript = writeScript "find-references" ''
-                    # Allow package names to be given directly, one per line (limit to 128
-                    # chars to avoid craziness)
-                    INPUT=$(cat)
-                    echo "$INPUT" | cut -c1-128
+      findHsPkgReferences =
+        let extractionScript = writeScript "find-references" ''
+              # Allow package names to be given directly, one per line (limit to 128
+              # chars to avoid craziness)
+              INPUT=$(cat)
+              echo "$INPUT" | cut -c1-128
 
-                    # Take package names from JSON fields. These include:
-                    #
-                    #  - Objects with a 'package' field
-                    #  - Arrays of such objects
-                    #  - Arrays of arrays of such objects
-                    #
-                    # We should be able to ignore dependencies, as they'll be brought in
-                    # automatically.
-                    FLATTEN='if type == "array" then .[] else .'
-                    echo "$INPUT" | jq -r "$FLATTEN | $FLATTEN | .package" 2> /dev/null ||
-                      true
-                  '';
+              # Take package names from JSON fields. These include:
+              #
+              #  - Objects with a 'package' field
+              #  - Arrays of such objects
+              #  - Arrays of arrays of such objects
+              #
+              # We should be able to ignore dependencies, as they'll be brought in
+              # automatically.
+              FLATTEN='if type == "array" then .[] else .'
+              echo "$INPUT" | jq -r "$FLATTEN | $FLATTEN | .package" 2> /dev/null ||
+                true
+            '';
 
-                  hsPkgNames = writeScript "haskell-names"
-                                 (concatStringsSep "\n" (attrNames haskellPackages));
-               in writeScript "unique-references" ''
-                    INPUT=$(cat | grep '[a-zA-Z_]')
-                    while read -r NAME
-                    do
-                      if grep -xF "$NAME" < "${hsPkgNames}" > /dev/null
-                      then
-                        echo "$NAME"
-                      fi
-                    done < <(echo "$INPUT" | "${extractionScript}" | sort -u | grep '^.')
-                  '';
-          };
-          script = ''
-            #!/usr/bin/env bash
-            set -e
-            set -o pipefail
-
-            function ensurePkg {
-              # Skip empty lines
-              echo "$1" | grep '[a-zA-Z_]' > /dev/null || return 0
-
-              if ghc-pkg list "$1" | grep "$1" > /dev/null
-              then
-                return 0
-              fi
-
-              echo "Aborting. Didn't find Haskell package '$1' in" 1>&2
-              ghc-pkg list 1>&2
-              exit 1
-            }
-
-            # We must have ghc-pkg, or else we can't even check the others
-            command -v "ghc-pkg" > /dev/null 2>&1 || {
-              echo "No ghc-pkg command in environment" 1>&2
-              exit 1
-            }
-
-            # '|| true' to appease 'set -e' when we have no input
-            INPUT=""
-            [ -t 0 ] || INPUT=$(sort -u | grep "^." | cut -c1-128) || true
-
-            if [[ -n "$INPUT" ]]
-            then
-              while read -r PKG
+            hsPkgNames = writeScript "haskell-names"
+                           (concatStringsSep "\n" (attrNames haskellPackages));
+         in writeScript "unique-references" ''
+              INPUT=$(cat | grep '[a-zA-Z_]')
+              while read -r NAME
               do
-                ensurePkg "$PKG"
-              done < <(echo "$INPUT"    |
-                       grep '[a-zA-Z_]' |
-                       "$findHsPkgReferences")
-            fi
+                if grep -xF "$NAME" < "${hsPkgNames}" > /dev/null
+                then
+                  echo "$NAME"
+                fi
+              done < <(echo "$INPUT" | "${extractionScript}" | sort -u | grep '^.')
+            '';
+    };
+    script = ''
+      #!/usr/bin/env bash
+      set -e
+      set -o pipefail
 
-            while read -r PKG
-            do
-              ensurePkg "$PKG"
-            done < <(echo "$allGiven")
+      function ensurePkg {
+        # Skip empty lines
+        echo "$1" | grep '[a-zA-Z_]' > /dev/null || return 0
 
-            exit 0
-          '';
-        };
+        if ghc-pkg list "$1" | grep "$1" > /dev/null
+        then
+          return 0
+        fi
+
+        echo "Aborting. Didn't find Haskell package '$1' in" 1>&2
+        ghc-pkg list 1>&2
+        exit 1
+      }
+
+      # We must have ghc-pkg, or else we can't even check the others
+      command -v "ghc-pkg" > /dev/null 2>&1 || {
+        echo "No ghc-pkg command in environment" 1>&2
+        exit 1
+      }
+
+      # '|| true' to appease 'set -e' when we have no input
+      INPUT=""
+      [ -t 0 ] || INPUT=$(sort -u | grep "^." | cut -c1-128) || true
+
+      if [[ -n "$INPUT" ]]
+      then
+        while read -r PKG
+        do
+          ensurePkg "$PKG"
+        done < <(echo "$INPUT"    |
+                 grep '[a-zA-Z_]' |
+                 "$findHsPkgReferences")
+      fi
+
+      while read -r PKG
+      do
+        ensurePkg "$PKG"
+      done < <(echo "$allGiven")
+
+      exit 0
+    '';
+  };
 
   # Run commands with extra info for debugging and reproducability:
   #  - Store the given command, arguments, stdin, stdout and stderr
