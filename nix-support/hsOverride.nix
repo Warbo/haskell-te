279ae3f2731e92c101ffb9a31227b7b7f69028b4
Chris Warburton <chriswarbo@gmail.com>
2017-10-08 13:07:44 +0100

    Remove duplicate URLs

--- nix-support/hsOverride.nix
+++ nix-support/hsOverride.nix
@@ -9,69 +9,71 @@ with rec {
 
     AstPlugin = get {
       path   = <ast-plugin>;
-      url    = https://github.com/Warbo/ast-plugin.git;
+      repo   = "ast-plugin";
       rev    = "a04f6fe";
       sha256 = "1gmkv4l38vpvhg2h8dwv4gf8dq1d0lr0zxd5j9szi90xb8nl2241";
     } {};
 
     bench  = get {
-      url    = https://github.com/Gabriel439/bench.git;
+      owner  = "Gabriel439";
+      repo   = "bench";
       rev    = "2575ff3";
       sha256 = "18xplqhhqn9y7byib01sv75i4wbgg73rz53bn1ppd5mfxhdp8g8x";
     } {};
 
     GetDeps = get {
       path   = <get-deps>;
-      url    = https://github.com/Warbo/getDeps.git;
+      repo   = "getDeps";
       rev    = "7c02fbc";
       sha256 = "0iw9ajzmq439qipm5r21dby32xlmdxg8bnsm6bcgm8dr9whhalfl";
     } {};
 
     HS2AST = get {
       path   = <hs2ast>;
-      url    = https://github.com/Warbo/hs2ast.git;
+      repo   = "hs2ast";
       rev    = "469d999";
       sha256 = "1yancv9pd8rnkpla462czsi4kgd8nvndjixmr1s6kkc7xk68zky8";
     } {};
 
     ifcxt = get {
       path   = <ifcxt>;
-      url    = https://github.com/mikeizbicki/ifcxt.git;
+      owner  = "mikeizbicki";
+      repo   = "ifcxt";
       rev    = "7f9f876";
       sha256 = "1k267zs3w999xg90ddy02l4cpjn14x07cdgqbca3w0ncili2p7a2";
     } {};
 
     ML4HSFE = get {
       path   = <ml4hsfe>;
-      url    = https://github.com/Warbo/ml4hsfe.git;
+      repo   = "ml4hsfe";
       rev    = "bcdd93b";
       sha256 = "0rraj7ias334g39vyc5z9afms4w30g5vf1czgw7fskahiir2vmi1";
     } {};
 
     mlspec = get {
       path   = <mlspec>;
-      url    = https://github.com/Warbo/mlspec.git;
+      repo   = "mlspec";
       rev    = "a5f0dd2";
       sha256 = "1m2vs51wqdqlv67z1qcv3jhwapb9dp5k336vjvjkqpr8svg15yas";
     } {};
 
     mlspec-helper = get {
       path   = <mlspec-helper>;
-      url    = https://github.com/Warbo/mlspec-helper.git;
+      repo   = "mlspec-helper";
       rev    = "d794706";
       sha256 = "1vaniwziqq0w1ajrabxbyf76iqplk7c765z13k5kpndhmqmsga5r";
     } {};
 
     nix-eval = get {
       path   = <nix-eval>;
-      url    = https://github.com/Warbo/nix-eval.git;
+      repo   = "nix-eval";
       rev    = "53d18a2";
       sha256 = "1agw1zppbhgk6aaqd75mmvbd6r8d67ap4y5rli1sm1j1q0a5qqx5";
     } {};
 
     reduce-equations = get {
       path   = <reduce-equations>;
-      url    = https://github.com/Warbo/reduce-equations.git;
+      repo   = "reduce-equations";
       rev    = "a86199b";
       sha256 = "11dafq0zl6vj1lzybsfpryvc2rqbxxkjjy3v4ab1vb8nzhxfgna3";
     } {
@@ -82,19 +84,20 @@ with rec {
 
     runtime-arbitrary = get {
       path   = <runtime-arbitrary>;
-      url    = https://github.com/Warbo/runtime-arbitrary;
+      repo   = "runtime-arbitrary";
       rev    = "5b7ff2f";
       sha256 = "11gnfmz48vxvf42xs9255r51vbv1sjghvzi60gcrpx3jk38d2gyb";
     } {};
 
     spoon = get {
-      url    = https://github.com/Warbo/spoon.git;
+      repo   = "spoon";
       rev    = "4424f9a";
       sha256 = "1d14qawzf7pimkjccj3f2vy9nc4fb2w00ib8li4kb180fb6wjvq4";
     } {};
 
     weigh = get {
-      url    = https://github.com/fpco/weigh.git;
+      owner  = "fpco";
+      repo   = "weigh";
       rev    = "26f8e3e";
       sha256 = "0pmkzlcjfqi41qmrgjyw1y7naclq86kb6mp0i4ni3d1lkiylb9gc";
     } {};
@@ -104,12 +107,16 @@ with rec {
 self: super: hsPkgs {
   hackagePkg = n: v: self.callPackage (callHackage n v);
 
-  get = { path ? null, url, rev, sha256}:
+  get = { path ? null, url ? null, owner ? "Warbo", repo ? null, rev, sha256}:
+    assert url == null -> repo != null || abort "Need URL or repo (${sha256})";
     with rec {
-      stable = { inherit rev sha256; };
-      git    = nix-config.latestGit { inherit stable url; };
-      src    = with tryEval path;
-               if success && value != null then value else git;
+      stable  = { inherit rev sha256; };
+      fullUrl = if url == null
+                   then "https://github.com/${owner}/${repo}.git"
+                   else url;
+      git     = nix-config.latestGit { inherit stable; url = fullUrl; };
+      src     = with tryEval path;
+                if success && value != null then value else git;
     };
     self.callPackage (nixFromCabal (toString src) null);
 }
