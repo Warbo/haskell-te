e19ff9b982fdb026bdd1204b3251e77c1ac1e620
Chris Warburton <chriswarbo@gmail.com>
2017-10-10 02:03:15 +0100

    Make more tests into dependencies of their units

--- nix-support/genQuickspecRunner.nix
+++ nix-support/genQuickspecRunner.nix
@@ -1,6 +1,7 @@
 { bash, bashEscape, checkStderr, fail, gnugrep, gnused, haskellPackages,
-  haskellPkgNameVersion, haveVar, jq, makeWrapper, mkBin, nix, nixEnv,
-  pipeToNix, timeout, wrap, writeScript }:
+  haskellPkgNameVersion, haveVar, jq, makeWrapper, mkBin, nix, nixEnv, nixify,
+  pipeToNix, runCommand, testData, timeout, withDeps, withNix, wrap,
+  writeScript }:
 
 with rec {
   getCmd = wrap {
@@ -21,18 +22,18 @@ with rec {
                         withoutUndef' (renderWithVariables x ts)  ++
                         "); mapM_ print eqs; }"
 
-        -- Reads JSON from stdin, outputs a QuickSpec signature and associated shell
-        -- and Nix commands for running it
+        -- Reads JSON from stdin, outputs a QuickSpec signature and associated
+        -- shell and Nix commands for running it
         main = do
           projects <- getProjects <$> getContents
           let t = case projects of
-                       [t] -> t
-                       _   -> error ("Got " ++ show (length projects) ++ " projects")
+            [t] -> t
+            _   -> error ("Got " ++ show (length projects) ++ " projects")
 
           rendered <- renderTheory t
           let (ts, x) = case rendered of
-                             Nothing      -> error ("Failed to render " ++ show t)
-                             Just (ts, x) -> (ts, x)
+            Nothing      -> error ("Failed to render " ++ show t)
+            Just (ts, x) -> (ts, x)
 
           BS.putStrLn (encode (object [
               "runner" .= unwords ("runhaskell" : flagsOf x),
@@ -157,5 +158,121 @@ with rec {
       nix-build --no-out-link --show-trace -E 'import (builtins.getEnv "mkCmd")'
     '';
   };
+
+  tests = rec {
+    runner = runCommand "list-full-runner"
+      (withNix {
+        asts        = testData.asts.list-full;
+        OUT_DIR     = nixify testData.haskellPkgs.list-full;
+        buildInputs = [ generateCode ];
+      })
+      ''
+        R=$(genQuickspecRunner < "$asts")
+        ln -s "$R" "$out"
+      '';
+
+    env = runCommand "list-full-env" { inherit runner; } ''
+      grep -v '^exec ' < "$runner" > "$out"
+    '';
+
+    code = runCommand "code.hs"
+      {
+        inherit env;
+        buildInputs = [ fail ];
+      }
+      ''
+        source "$env"
+        [[ -e "$HASKELL_CODE" ]] || fail "HASKELL_CODE ($HASKELL_CODE) not found"
+        ln -s "$HASKELL_CODE" "$out"
+      '';
+
+    countVars = with { ticks = "''"; }; writeScript "countVars.hs" ''
+      -- TODO: We don't need all of these
+      import Test.QuickSpec hiding (lists)
+      import Test.QuickSpec.Signature
+      import Data.Digest.Murmur32
+      import Data.Serialize
+      import MLSpec.Helper
+      import A
+      import IfCxt
+      import Test.QuickCheck
+      import Test.RuntimeArbitrary
+      import Prelude
+      import GHC.Types
+      import qualified Test.Feat as F
+
+      mkIfCxtInstances ${ticks}F.Enumerable
+      mkIfCxtInstances ${ticks}Ord
+      mkIfCxtInstances ${ticks}CoArbitrary
+      mkIfCxtInstances ${ticks}Arbitrary
+
+      func1 = ("Unary",   Test.RuntimeArbitrary.getArbGen
+                            [Prelude.undefined :: ((->) Prelude.Integer
+                                                        Prelude.Integer)])
+      func2 = ("Binary",  Test.RuntimeArbitrary.getArbGen
+                            [Prelude.undefined :: ((->) Prelude.Integer
+                                                        ((->) Prelude.Integer
+                                                              Prelude.Integer))])
+      ints  = ("Integer", Test.RuntimeArbitrary.getArbGen
+                            [Prelude.undefined :: (Prelude.Integer)])
+      lists = ("List",    Test.RuntimeArbitrary.getArbGen
+                            [Prelude.undefined :: ((A.List) Prelude.Integer)])
+      nats  = ("Nat",     Test.RuntimeArbitrary.getArbGen
+                            [Prelude.undefined :: (A.Nat)])
+
+      check (t, l) = case l of
+        [] -> error ("No Arbitrary instance for " ++ t)
+        _  -> return ()
+
+      main = sequence [
+          check ints
+        , check nats
+        , check lists
+        , check func1
+        , check func2
+        , putStrLn "Found Arbitrary instances"
+        ]
+    '';
+
+    askForVariables = runCommand "ask-for-vars"
+      {
+        inherit code;
+        buildInputs = [ fail ];
+      }
+      ''
+        set -e
+        echo "Checking Haskell code requests vars for appropriate types" 1>&2
+
+        # Find where we're adding variables to the signature and get their types
+        # This parsing is pretty fragile; if it breaks, it might be worth using
+        # haskell-src-exts or similar.
+        TYPES=$(grep -A 2 'MLSpec.Helper.addVars' < "$code" |
+                grep 'getArbGen'                            |
+                grep -o ':: .*\]'                           |
+                grep -o ' .*[^]]'                           |
+                grep -o '[^ ].*[^ ]'                        )
+
+        for TYPE in Prelude.Integer '(A.List) Prelude.Integer'
+        do
+          echo "$TYPES" | grep -F "$TYPE" > /dev/null ||
+            fail "Didn't ask for variables of type '$TYPE'"
+        done
+
+        echo "pass" > "$out"
+      '';
+
+    haveGenerators = runCommand "have-generators"
+      {
+        inherit countVars env;
+        buildInputs = [ generateCode ];
+      }
+      ''
+        set -e
+        echo "Checking that we can find Arbitrary instances" 1>&2
+        source "$env"
+        $CMD < "$countVars"
+        echo "pass" > "$out"
+      '';
+  };
 };
-generateCode
+withDeps [ tests.askForVariables tests.haveGenerators ] generateCode
