88154a8a3783b8dd54f9739f2163d17249016210
Chris Warburton <chriswarbo@gmail.com>
2017-10-11 00:20:33 +0100

    Move equation tests to package

--- nix-support/package.nix
+++ nix-support/package.nix
@@ -1,7 +1,80 @@
-{ buckets, buildEnv, hashspecBench, haskellPkgToAsts, mlspecBench, quickspec,
-  quickspecAsts, stdenv, tipBenchmarks }:
+{ buckets, buildEnv, fail, filterToSampled, genQuickspecRunner, hashspecBench,
+  haskellPkgToAsts, jq, mlspecBench, nixify, quickspec, quickspecAsts,
+  runCommand, stdenv, testData, tipBenchmarks, withDeps, withNix, writeScript }:
 
-buildEnv {
+with rec {
+  dep   = "global746970323031352f62696e5f646973747269622e736d7432706c7573";
+  tests = rec {
+    checkParamTypes = runCommand "can-find-properties-of-parameterised-types"
+      (withNix {
+        buildInputs  = [ fail jq tipBenchmarks.tools ];
+        eqs          = testData.eqs.list-full;
+        GROUND_TRUTH = ../benchmarks/ground-truth/list-full.smt2;
+        TRUTH_SOURCE = ../benchmarks/ground-truth/list-full.smt2;
+      })
+      ''
+        set -e
+        set -o pipefail
+        RESULT=$(echo "$eqs" | precision_recall_eqs)
+        echo "$RESULT" | jq -e '.recall | . > 0' || fail "No recall"
+        mkdir "$out"
+      '';
+
+    canFindComm = runCommand "can-find-commutativity"
+      {
+        precRec = runCommand "pr"
+          {
+            commEqs = runCommand "commutativityEqs.json"
+              {
+                commRunner = runCommand "commutativityExplorer"
+                  {
+                    commDeps = runCommand "commDeps"
+                      (tipBenchmarks.cache // {
+                        getCommDeps = writeScript "getCommDeps.rkt" ''
+                          #lang racket
+                          (require (file "${tipBenchmarks.path}/scripts/lib/normalise.rkt"))
+                          (require (file "${tipBenchmarks.path}/scripts/lib/theorems.rkt"))
+                          (for ([dep (theorem-deps-of "tip2015/bin_plus_comm.smt2")])
+                               (write (encode-lower-name dep)))
+                        '';
+                        buildInputs = [ tipBenchmarks.env ];
+                      })
+                      ''
+                        racket "$getCommDeps" > "$out"
+                      '';
+                    asts        = testData.asts.teBenchmark;
+                    OUT_DIR     = nixify testData.haskellPkgs.teBenchmark;
+                    buildInputs = [ filterToSampled genQuickspecRunner jq ];
+                  }
+                  ''
+                    SAMPLE=$(cat "$commDeps")
+                    export SAMPLE
+
+                    R=$(filterToSampled < "$asts" | genQuickspecRunner)
+                    ln -s "$R" "$out"
+                  '';
+              }
+              ''
+                "$commRunner" > "$out"
+              '';
+            SAMPLED_NAMES = dep;
+            buildInputs   = [ tipBenchmarks.tools ];
+          }
+          ''
+            decode < "$commEqs" | conjectures_for_sample > "$out"
+          '';
+        buildInputs = [ jq ];
+      }
+      ''
+        set -e
+        jq -e '.precision | . > 0' < "$precRec"
+        jq -e '.recall    | . > 0' < "$precRec"
+        mkdir "$out"
+      '';
+    };
+};
+
+withDeps [ tests.checkParamTypes tests.canFindComm ] (buildEnv {
   name  = "haskell-theory-exploration";
   paths = [
     quickspec
@@ -9,4 +82,4 @@ buildEnv {
     haskellPkgToAsts
     mlspecBench.mls
   ];
-}
+})
--- nix-support/tipBenchmarks.nix
+++ nix-support/tipBenchmarks.nix
@@ -1,9 +1,7 @@
-{ annotated, asv-nix, bash, callPackage, fetchFromGitHub, fetchgit,
-  haskellPackages, jq, nix-config, nix-config-src, nixFromCabal, nixpkgs-src,
-  runCommand, stable, stdenv, tryElse, writeScript }:
+{ asv-nix, callPackage, haskellPackages, nix-config, nix-config-src, tryElse }:
 
 with builtins;
-with rec {
+with {
   path = tryElse <te-benchmarks> (nix-config.latestGit {
     url    = http://chriswarbo.net/git/theory-exploration-benchmarks.git;
     stable = {
@@ -11,89 +9,10 @@ with rec {
       sha256 = "1isbzv29903fh3m1sikj6gyaylq6wcw042wxna1g6k8wnlac9xjb";
     };
   });
-
-  tebench  = callPackage path {
-    inherit asv-nix haskellPackages nix-config-src;
-
-    # Nixpkgs 17.03 disables Racket on i686, so always use 16.09 (for now)
-    pkgsPath = nix-config.repo1609;
-  };
-
-  # Tests
-
-  dep = "global746970323031352f62696e5f646973747269622e736d7432706c7573";
-
-  canFindCommutativity = runCommand "can-find-commutativity"
-    {
-      precRec = runCommand "pr"
-        {
-          commEqs = runCommand "commutativityEqs.json"
-            {
-              commRunner = runCommand "commutativityExplorer"
-                {
-                  commDeps = runCommand "commDeps"
-                    (tebench.cache // {
-                      getCommDeps = writeScript "getCommDeps.rkt" ''
-                        #lang racket
-                        (require (file "${path}/scripts/lib/normalise.rkt"))
-                        (require (file "${path}/scripts/lib/theorems.rkt"))
-                        (for ([dep (theorem-deps-of "tip2015/bin_plus_comm.smt2")])
-                             (write (encode-lower-name dep)))
-                      '';
-                      buildInputs = [ tebench.env ];
-                    })
-                    ''
-                      racket "$getCommDeps" > "$out"
-                    '';
-                  asts        = testData.asts.teBenchmark;
-                  OUT_DIR     = nixify testData.haskellPkgs.teBenchmark;
-                  buildInputs = [ filterToSampled genQuickspecRunner jq ];
-                }
-                ''
-                  SAMPLE=$(cat "$commDeps")
-                  export SAMPLE
-
-                  R=$(filterToSampled < "$asts" | genQuickspecRunner)
-                  ln -s "$R" "$out"
-                '';
-            }
-            ''
-              "$commRunner" > "$out"
-            '';
-          SAMPLED_NAMES = dep;
-          buildInputs   = [ tebench.tools ];
-        }
-        ''
-          decode < "$commEqs" | conjectures_for_sample > "$out"
-        '';
-      buildInputs = [ jq ];
-    }
-    ''
-      set -e
-      jq -e '.precision | . > 0' < "$precRec"
-      jq -e '.recall    | . > 0' < "$precRec"
-      mkdir "$out"
-    '';
-
-  checkParamTypes = runCommand "can-find-properties-of-parameterised-types"
-    (withNix {
-      buildInputs  = [ fail jq tebench.tools ];
-      eqs          = testData.eqs.list-full;
-      GROUND_TRUTH = ../benchmarks/ground-truth/list-full.smt2;
-      TRUTH_SOURCE = ../benchmarks/ground-truth/list-full.smt2;
-    })
-    ''
-      set -e
-      set -o pipefail
-      RESULT=$(echo "$eqs" | precision_recall_eqs)
-      echo "$RESULT" | jq -e '.recall | . > 0' || fail "No recall"
-      mkdir "$out"
-    '';
 };
-rec {
-  inherit (tebench)
-    tip-benchmarks cache env tools tip-benchmark-haskell tip-benchmark-smtlib;
-  annotatedAsts = annotated { pkgDir = tip-benchmark-haskell; };
-  pkg           = haskellPackages.callPackage pkgDef {};
-  pkgDef        = nixFromCabal (toString tip-benchmark-haskell) null;
+{ inherit path; } // callPackage path {
+  inherit asv-nix haskellPackages nix-config-src;
+
+  # Nixpkgs 17.03 disables Racket on i686, so always use 16.09 (for now)
+  pkgsPath = nix-config.repo1609;
 }
