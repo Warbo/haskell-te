c542919110bf5588720b8304d5e1978ad8695f3c
Chris Warburton <chriswarbo@gmail.com>
2017-10-10 19:21:18 +0100

    Replace uses of testRun with runCommand

--- tests/hashspecBench.nix
+++ tests/hashspecBench.nix
@@ -1,18 +1,13 @@
 defs: with defs; with lib; with builtins;
 
 with rec {
-  maxSecs = "300";
-
-  fail = msg: ''{ echo -e "${msg}" 1>&2; exit 1; }'';
-
+  maxSecs  = "300";
   testFile = name: path: runCommand "mls-${name}"
     {
-      buildInputs = [ jq package ];
+      buildInputs = [ fail jq package ];
+      MAX_SECS    = maxSecs;
     }
     ''
-      #!${bash}/bin/bash
-      export MAX_SECS="${maxSecs}"
-
       echo "Running ${name} through hashspecBench" 1>&2
       OUTPUT=$(hashspecBench < "${path}") || {
         echo "Couldn't explore ${name}" 1>&2
@@ -20,17 +15,13 @@ with rec {
       }
 
       T=$(echo "$OUTPUT" |
-          jq 'has("cmd") and has("info") and has("results")') || {
-        echo -e "Couldn't parse output\nSTART\n$OUTPUT\nEND" 1>&2
-        exit 1
-      }
+          jq 'has("cmd") and has("info") and has("results")') ||
+        fail "Couldn't parse output\nSTART\n$OUTPUT\nEND"
 
-      [[ "x$T" = "xtrue" ]] || {
-        echo -e "Required fields missing:\n$OUTPUT" 1>&2
-        exit 1
-      }
+      [[ "x$T" = "xtrue" ]] ||
+        fail "Required fields missing:\n$OUTPUT"
 
-      echo "Pass" > "$out"
+      mkdir "$out"
     '';
 };
 
@@ -38,84 +29,83 @@ with rec {
   list-full  = testFile "list-full"  ../benchmarks/list-full.smt2;
   nat-full   = testFile "nat-full"   ../benchmarks/nat-full.smt2;
   nat-simple = testFile "nat-simple" ../benchmarks/nat-simple.smt2;
-} // mapAttrs (name: testRun name null {
-                  buildInputs = [ package tipBenchmarks.tools ];
+} // mapAttrs (name: runCommand name {
+                  buildInputs = [ fail package tipBenchmarks.tools ];
+                  MAX_SECS    = maxSecs;
                 }) {
   canRun = ''
-    export MAX_SECS="${maxSecs}"
-    hashspecBench < ${./example.smt2} || exit 1
+    set -e
+    hashspecBench < "${./example.smt2}"
+    mkdir "$out"
   '';
 
   outputIsJson = ''
     set -e
-    export MAX_SECS="${maxSecs}"
-    OUTPUT=$(hashspecBench < ${./example.smt2}) || exit 1
-    TYPE=$(echo "$OUTPUT" | jq -r 'type') || {
-      echo -e "START OUTPUT\n$OUTPUT\nEND OUTPUT" 1>&2
-      exit 1
-    }
-    [[ "x$TYPE" = "xobject" ]] || {
-      echo -e "START OUTPUT\n$OUTPUT\nEND OUTPUT" 1>&2
-      echo "Type is '$TYPE' instead of object" 1>&2
-      exit 1
-    }
+    OUTPUT=$(hashspecBench < ${./example.smt2})
+
+    TYPE=$(echo "$OUTPUT" | jq -r 'type') ||
+      fail "START OUTPUT\n$OUTPUT\nEND OUTPUT"
+
+    [[ "x$TYPE" = "xobject" ]] ||
+      fail "START OUTPUT\n$OUTPUT\nEND OUTPUT\nGot '$TYPE' instead of object"
+
+    mkdir "$out"
   '';
 
   haveEquations = ''
     set -e
-    export MAX_SECS="${maxSecs}"
-    OUTPUT=$(hashspecBench < ${./example.smt2})   || exit 1
+    OUTPUT=$(hashspecBench < ${./example.smt2})    || exit 1
      CHECK=$(echo "$OUTPUT" | jq 'has("results")') || exit 1
-    [[ "x$CHECK" = "xtrue" ]] || {
-      echo -e "Didn't find 'results' in\n$OUTPUT" 1>&2
-      exit 1
-    }
+    [[ "x$CHECK" = "xtrue" ]] ||
+      fail "Didn't find 'results' in\n$OUTPUT"
+    mkdir "$out"
   '';
 
-  filterSamples = let keepers = [
-    { name = "append";          module = "A"; package = "tip-benchmark-sig"; }
-    { name = "constructorNil";  module = "A"; package = "tip-benchmark-sig"; }
-    { name = "constructorCons"; module = "A"; package = "tip-benchmark-sig"; }
-  ];
-  in ''
-       set -e
-       export MAX_SECS="${maxSecs}"
+  filterSamples =
+    with {
+      keepers = map (name: {
+                      inherit name;
+                      module  = "A";
+                      package = "tip-benchmark-sig";
+                    }) [ "append" "constructorNil" "constructorCons" ];
+    };
+    ''
+      set -e
 
-       BENCH_OUT=$(CLUSTERS=1 SAMPLE_SIZES="5" hashspecBench)
+      BENCH_OUT=$(CLUSTERS=1 SAMPLE_SIZES="5" hashspecBench)
 
-       # Get all the constant symbols in all equations
-       STDOUTS=$(echo "$BENCH_OUT" | jq -r '.results | .[] | .stdout') ||
-         ${fail "Couldn't get stdouts\n\n$BENCH_OUT"}
+      # Get all the constant symbols in all equations
+      STDOUTS=$(echo "$BENCH_OUT" | jq -r '.results | .[] | .stdout') ||
+        fail "Couldn't get stdouts\n\n$BENCH_OUT"
 
-       OUTPUTS=$(while read -r F
-                 do
-                   cat "$F"
-                 done < <(echo "$STDOUTS")) ||
-         ${fail "Couldn't concat stdouts\n\n$BENCH_OUT\n\n$STDOUTS"}
+      OUTPUTS=$(while read -r F
+                do
+                  cat "$F"
+                done < <(echo "$STDOUTS")) ||
+        fail "Couldn't concat stdouts\n\n$BENCH_OUT\n\n$STDOUTS"
 
-       EQS=$(echo "$OUTPUTS" | grep -v '^Depth') ||
-         ${fail "Couldn't get eqs\n\n$BENCH_OUT\n\n$OUTPUTS"}
+      EQS=$(echo "$OUTPUTS" | grep -v '^Depth') ||
+        fail "Couldn't get eqs\n\n$BENCH_OUT\n\n$OUTPUTS"
 
-       NAMES=$(echo "$EQS" |
-               jq -r 'getpath(paths(type == "object" and .role == "constant")) |
-                      .symbol' |
-               sort -u) || ${fail "Couldn't get names\n\n$BENCH_OUT\n\n$EQS"}
-       SAMPLE=$(choose_sample 5 1)
+      NAMES=$(echo "$EQS" |
+              jq -r 'getpath(paths(type == "object" and .role == "constant")) |
+                     .symbol' |
+              sort -u) || fail "Couldn't get names\n\n$BENCH_OUT\n\n$EQS"
+      SAMPLE=$(choose_sample 5 1)
 
-       # Remove any names which appear in the sample
-       while read -r NAME
-       do
-         NAMES=$(echo "$NAMES" | grep -vFx "$NAME") || true
-       done < <(echo "$SAMPLE")
+      # Remove any names which appear in the sample
+      while read -r NAME
+      do
+        NAMES=$(echo "$NAMES" | grep -vFx "$NAME") || true
+      done < <(echo "$SAMPLE")
 
-       # If there are any names remaining, they weren't in the sample
-       if echo "$NAMES" | grep '^.' > /dev/null
-       then
-         echo "Found names which aren't in sample" 1>&2
-         echo -e "NAMES:\n$NAMES\n\nOUTPUT:\n$BENCH_OUT\nSAMPLE:\n$SAMPLE" 1>&2
-         exit 1
-       fi
+      # If there are any names remaining, they weren't in the sample
+      if echo "$NAMES" | grep '^.' > /dev/null
+      then
+        DBG="NAMES:\n$NAMES\n\nOUTPUT:\n$BENCH_OUT\nSAMPLE:\n$SAMPLE"
+        fail "Found names which aren't in sample\n$DBG"
+      fi
 
-       echo "Passed" > "$out"
-  '';
+      mkdir "$out"
+    '';
 }
