114bbdaa1fa245a762443cc0f7a4c8e26f123ec7
Chris Warburton <chriswarbo@gmail.com>
2017-10-12 18:29:25 +0100

    Simplify test

--- nix-support/analysis.nix
+++ nix-support/analysis.nix
@@ -1,52 +1,44 @@
 # Scripts, mostly from tipBenchmarks, which are useful for analysing output
-{ attrsToDirs, filterToSampled, genQuickspecRunner, jq, lib, runCommand,
+{ attrsToDirs, fail, filterToSampled, genQuickspecRunner, jq, lib, runCommand,
   testData, tipBenchmarks, withDeps, wrap }:
 
 with rec {
   scripts = attrsToDirs {
-    bin = lib.genAttrs [ "choose_sample" "conjectures_for_sample" ]
+    bin = lib.genAttrs [ "choose_sample" "decode" "conjectures_for_sample" ]
                        (name: wrap {
                          inherit name;
                          file = "${tipBenchmarks.tools}/bin/${name}";
-                       });
+                       }) // {
+      conjectures_for_sample = wrap {
+        name = "conjectures_for_sample";
+        paths  = [ tipBenchmarks.tools ];
+        script = ''
+          #!/usr/bin/env bash
+          decode | conjectures_for_sample
+        '';
+      };
+    };
   };
 
   canFindComm = runCommand "can-find-commutativity"
     {
-      precRec = runCommand "pr"
-        {
-          commEqs = runCommand "commutativityEqs.json"
-            {
-              commRunner = runCommand "commutativityExplorer"
-                {
-                  inherit (tipBenchmarks) commDeps;
-                  asts        = (testData.asts         {}).teBenchmark;
-                  OUT_DIR     = (testData.haskellNixed {}).teBenchmark;
-                  buildInputs = [ filterToSampled genQuickspecRunner jq ];
-                }
-                ''
-                  SAMPLE=$(cat "$commDeps")
-                  export SAMPLE
-
-                  R=$(filterToSampled < "$asts" | genQuickspecRunner)
-                  ln -s "$R" "$out"
-                '';
-            }
-            ''
-              "$commRunner" > "$out"
-            '';
-          SAMPLED_NAMES = "global746970323031352f62696e5f646973747269622e736d7432706c7573";
-          buildInputs   = [ tipBenchmarks.tools ];
-        }
-        ''
-          decode < "$commEqs" | conjectures_for_sample > "$out"
-        '';
-      buildInputs = [ fail jq ];
+      inherit (tipBenchmarks) commDeps;
+      buildInputs   = [ fail filterToSampled genQuickspecRunner jq scripts ];
+      asts          = (testData.asts         {}).teBenchmark;
+      OUT_DIR       = (testData.haskellNixed {}).teBenchmark;
+      SAMPLED_NAMES = "global746970323031352f62696e5f646973747269622e736d7432706c7573";
     }
     ''
       set -e
-      jq -e '.precision | . > 0' < "$precRec" || fail "No/zero precision"
-      jq -e '.recall    | . > 0' < "$precRec" || fail "No/zero recall"
+      SAMPLE=$(cat "$commDeps")
+      export SAMPLE
+
+      FLT=$(filterToSampled < "$asts")            || fail "Couldn't filter"
+      RUN=$(echo "$FLT" | genQuickspecRunner)     || fail "Couldn't gen runner"
+      EQS=$("$RUN")                               || fail "Couldn't gen eqs"
+      CON=$(echo "$EQS" | conjectures_for_sample) || fail "Couldn't look up eqs"
+      echo "$CON" | jq -e '.precision | . > 0'    || fail "No/zero precision"
+      echo "$CON" | jq -e '.recall    | . > 0'    || fail "No/zero recall"
       mkdir "$out"
     '';
 };
