9c7d0eaa846e83f867e6760f4f38c2b0293b8307
Chris Warburton <chriswarbo@gmail.com>
2017-10-28 15:58:48 +0100

    Prevent 'file exists' in benchmarkEnv

--- benchmarkEnv.nix
+++ benchmarkEnv.nix
@@ -1,17 +1,17 @@
 with import ./nix-support {};
 stdenv.mkDerivation {
-  name        = "haskell-te-benchmark-env";
-  src         = ./benchmarkEnv.nix;
-  unpackPhase = "true";
-  buildInputs = [ asv-nix fail ];
-  buildPhase  = ''
+  name         = "haskell-te-benchmark-env";
+  src          = ./benchmarkEnv.nix;
+  unpackPhase  = "true";
+  buildInputs  = [ asv-nix fail ];
+  installPhase = ''
     set -e
     echo 'WARNING: Building the "benchmarkEnv" derivation. This is not meant
     to be a "real" package: it only provides a build environment, for use with
     nix-shell (e.g. "nix-shell benchmarkEnv.nix")'
     echo "This is not a 'package', it's meant to be used via nix-shell" > "$out"
   '';
-  shellHook   = ''
+  shellHook = ''
     echo "Entered benchmarking shell: use 'asv' command to run"
   '';
 }
