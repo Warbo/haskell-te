1727d48d17b20f3575492837aa9bc32895a3f132
Chris Warburton <chriswarbo@gmail.com>
2017-11-08 22:04:31 +0000

    renderEqs now names variables unambiguously

--- nix-support/renderEqs.py
+++ nix-support/renderEqs.py
@@ -2,20 +2,56 @@
 from json import loads
 from sys  import stdin
 
-def wrap(expr):
+def dedupe(l):
+    result = []
+    for x in l:
+        if x not in result:
+            result += [x]
+    return result
+
+def names(expr):
+    def go(x):
+        return {
+            'constant'    : lambda: [x['symbol']],
+            'variable'    : lambda: [],
+            'application' : lambda: go(x['lhs']) + go(x['rhs'])
+        }[x['role']]()
+    return go(expr)
+
+def varsOf(lhs, rhs):
+    def go(x):
+        return {
+            'constant'    : lambda: [],
+            'variable'    : lambda: [x],
+            'application' : lambda: go(x['lhs']) + go(x['rhs'])
+        }[x['role']]()
+    return dedupe(go(lhs) + go(rhs))
+
+def wrap(vs, expr):
     '''Wraps the given expression in parentheses if it's an application.'''
     return ('({0})' if expr['role'] == 'application' else '{0}').format(
-        render(expr))
+        render(vs, expr))
 
-def render(expr):
-    '''Render a single expression to a string.'''
+# Read in all equations
+eqs = loads(stdin.read())
+
+# Pick a symbol for variables which doesn't clash with any constant name
+syms   = dedupe(reduce(lambda r, eq: r + names(eq['lhs']) + names(eq['rhs']),
+                       eqs,
+                       []))
+varPre = 'v'
+while any([s.startswith(varPre) for s in syms]):
+    varPre += 'v'
+
+def render(vs, expr):
+    '''Render an expression to a string, numbering variables based on vs.'''
     return {
         'constant'    : lambda: expr['symbol'],
-        'variable'    : lambda: (filter(lambda c: c.isupper(), expr['type']) +
-                                 str(expr['id'])).lower(),
-        'application' : lambda: '{0} {1}'.format(wrap(expr['lhs']),
-                                                 wrap(expr['rhs']))
+        'variable'    : lambda: varPre + str(vs.index(expr)),
+        'application' : lambda: '{0} {1}'.format(wrap(vs, expr['lhs']),
+                                                 wrap(vs, expr['rhs']))
     }[expr['role']]()
 
-for eq in loads(stdin.read()):
-    print('{0} ~= {1}'.format(render(eq['lhs']), render(eq['rhs'])))
+for eq in eqs:
+    vs = varsOf(eq['lhs'], eq['rhs'])
+    print('{0} ~= {1}'.format(render(vs, eq['lhs']), render(vs, eq['rhs'])))
