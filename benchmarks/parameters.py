781e1e1b3da0d33f3c7516d347f4886d62edf89a
Chris Warburton <chriswarbo@gmail.com>
2018-03-11 15:51:05 +0000

    Allow specific reps to be run

--- benchmarks/parameters.py
+++ benchmarks/parameters.py
@@ -1,9 +1,10 @@
 from json import loads
 from os   import getenv
 
-parameters   = loads(getenv('parameters'))
-repetitions  = parameters['repetitions']
-timeout_secs = parameters['timeout_secs']
-max_size     = parameters['max_size']
-reps         = range(0, repetitions)
-sizes        = filter(lambda x: x % 2 == 1, range(1, max_size + 1))
+parameters    = loads(getenv('parameters'))
+repetitions   = parameters['repetitions']
+timeout_secs  = parameters['timeout_secs']
+max_size      = parameters['max_size']
+specific_reps = parameters['specific_reps']
+reps          = range(0, repetitions) if specific_reps == [] else specific_reps
+sizes         = range(1, max_size + 1)
