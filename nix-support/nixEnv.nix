3c2412b0b453697eac771bfea72957d65c09683b
Chris Warburton <chriswarbo@gmail.com>
2018-07-23 19:09:09 +0100

    Take more withNix stuff from nix-helpers and nixEnv from withNix
    The definition of withNix in nix-helpers has been updated to include a
    workaround for Nix 2.x, which our definition didn't have. We replace most of our
    definition (especially NIX_REMOTE) with the one from nix-helpers.
    
    We also define nixEnv as (withNix {}), rather than having withNix append nixEnv
    to its result. This removes some complication.

--- nix-support/nixEnv.nix
+++ nix-support/nixEnv.nix
@@ -1,77 +1,7 @@
 # Env vars required for using Nix commands inside builders
-{ fail, jq, lib, nix, runCommand }:
-
-with builtins;
-with lib;
-with rec {
-
-  # Override <nixpkgs>, with <real> as a fallback. Use toString so the path
-  # appears as-is, rather than being added to the Nix store.
-  pathParts = [ "nixpkgs=${toString ./.}"
-                   "real=${toString pathReal}"
-                (getEnv "NIX_PATH") ];
-
-  # If we don't have <real> yet, use <nixpkgs>
-  pathReal = with tryEval <real>;
-             if success then value else <nixpkgs>;
-
-  remoteGiven = getEnv "NIX_REMOTE";
-
-  # Ensure we can write to the Nix store (or ask a builders to do so for us)
-  remoteForce = import (runCommand "nix-remote.nix" { buildInputs = [ nix ]; } ''
-    if nix-instantiate --read-write-mode --eval -E null 2> /dev/null
-    then
-      printf "\"$NIX_REMOTE\"" > "$out"
-    else
-      printf '"daemon"'        > "$out"
-    fi
-  '');
-
-  checkPath = runCommand "try-nix-path.nix"
-    {
-      inherit NIX_REMOTE;
-      NIX_PATH    = concatStringsSep ":" pathParts;
-      buildInputs = [ fail jq nix ];
-    }
-    ''
-      set -e
-
-      function go {
-        echo "Checking: $*" 1>&2
-        nix-instantiate --eval --read-write-mode -E \
-          "with builtins // { x = import <nixpkgs> { config = {}; }; }; $1" ||
-          fail "Failed:\nNIX_PATH: $NIX_PATH\nNIX_REMOTE: $NIX_REMOTE"
-        echo "Finished: $*" 1>&2
-      }
-
-      echo "Checking <nixpkgs> gets overridden" 1>&2
-      RESULT=$(go '<nixpkgs>')
-      echo "$RESULT" | grep "nix-support" > /dev/null ||
-        fail "Didn't see 'nix-support' in <nixpkgs> ($RESULT)"
-
-      echo "Checking <nixpkgs> isn't polluted by ~/.nixpkgs/config.nix" 1>&2
-      go 'assert !(x                 ? warbo-utilities); true'
-      go 'assert !(x.haskellPackages ? haskell-example); true'
-
-      echo "Checking <nixpkgs> has our custom definitions" 1>&2
-      go 'assert x ? wrap; true'
-      for P in mlspec mlspec-helper nix-eval runtime-arbitrary
-      do
-        go "assert x.haskellPackages ? $P; true"
-      done
-
-      echo "$NIX_PATH" | jq -R '.' > "$out"
-    '';
-
-  NIX_REMOTE = if remoteGiven == ""
-                  then remoteForce   # Nix is writable, or we need to force
-                  else remoteGiven;  # Propagate the existing value
-};
+{ withNix }:
 
 {
-  value = {
-    inherit NIX_REMOTE;
-    NIX_PATH = import checkPath;
-  };
+  value           = { inherit (withNix {}) NIX_REMOTE NIX_PATH; };
   removeOverrides = true;  # Since they can't be serialised into an environment
 }
--- nix-support/withNix.nix
+++ nix-support/withNix.nix
@@ -1,9 +1,75 @@
-{ bash, coreutils, jq, lib, nix, nixEnv, perl, procps, runCommand, utillinux }:
+{ bash, coreutils, fail, jq, lib, nix, nix-helpers, nixpkgs1803, perl, procps,
+  runCommand, utillinux }:
+
 with builtins;
-with nixEnv;
 with rec {
-  commonDeps = [ bash coreutils jq nix perl procps utillinux ];
+  # Allow given env to override us, but we must override buildInputs
+  go = env: nix-helpers.withNix ({ inherit NIX_PATH; } // env) // {
+    buildInputs = (env.buildInputs or []) ++ commonDeps;
+  };
+
+  # We include these for historical reasons
+  commonDeps = [ bash coreutils jq nixPkg perl procps utillinux ];
+
+  # Override NIX_PATH to take into account recursion using <real>
+
+  NIX_PATH = concatStringsSep ":" [
+    "nixpkgs=${toString ./.}"
+    "real=${toString pathReal}"
+    (getEnv "NIX_PATH")
+  ];
+
+  # If we don't have <real> yet, use <nixpkgs>
+  pathReal = with tryEval <real>;
+             if success then value else <nixpkgs>;
+
+  # Make sure we use a Nix package which corresponds to the system's
+
+  nixV1 = compareVersions builtins.nixVersion "2" == -1;
+
+  nixPkg = if nixV1 then nix else nixpkgs1803.nix;
+
+  # Tests
+
+  checkPath = runCommand "try-nix-path.nix"
+    (go { buildInputs = [ fail jq ]; })
+    ''
+      function go {
+        echo "Checking: $*" 1>&2
+        nix-instantiate --eval --read-write-mode -E \
+          "with builtins // { x = import <nixpkgs> { config = {}; overlays = []; }; }; $1" ||
+          fail "Failed:\nNIX_PATH: $NIX_PATH\nNIX_REMOTE: $NIX_REMOTE"
+        echo "Finished: $*" 1>&2
+      }
+
+      echo "Checking <nixpkgs> gets overridden" 1>&2
+      RESULT=$(go '<nixpkgs>')
+      echo "$RESULT" | grep "nix-support" > /dev/null ||
+        fail "Didn't see 'nix-support' in <nixpkgs> ($RESULT)"
+
+      echo "Checking <nixpkgs> isn't polluted by ~/.nixpkgs/config.nix" 1>&2
+      go 'assert !(x                 ? warbo-utilities); true'
+      go 'assert !(x.haskellPackages ? haskell-example); true'
+
+      echo "Checking <nixpkgs> has our custom definitions" 1>&2
+      go 'assert x ? wrap; true'
+      for P in mlspec mlspec-helper nix-eval runtime-arbitrary
+      do
+        go "assert x.haskellPackages ? $P; true"
+      done
+
+      echo "true" > "$out"
+    '';
 };
 
-# Allow env to override nixEnv, but we must override buildInputs
-env: nixEnv // env // { buildInputs = (env.buildInputs or []) ++ commonDeps; }
+assert nixV1 -> compareVersions nixPkg.version "2" == -1 || die {
+  inherit (nixPkg) version;
+  error = "Nix package doesn't have version 1.x";
+};
+assert nixV1 || compareVersions nix.version "2" == 1 || die {
+  inherit (nixPkg) version;
+  error = "Nix package should be higher than 2.0.0";
+};
+assert import checkPath;
+
+go
