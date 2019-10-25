5deb6ea48af81515977513232ba87aa1925f85fc
Chris Warburton <chriswarbo@gmail.com>
2019-10-25 18:04:30 +0100

    Pull renderEqs out into a separate package

--- nix-support/renderEqs.nix
+++ nix-support/renderEqs.nix
@@ -1,53 +1,10 @@
-{ mkBin, python, runCommand, withDeps }:
+{ callPackage, fetchgit }:
 
-with rec {
-  untested = mkBin {
-    name  = "renderEqs";
-    paths = [ python ];
-    file  = ./renderEqs.py;
+with {
+  src = fetchgit {
+    url    = http://chriswarbo.net/git/render-eqs.git;
+    rev    = "2d1ff3f";
+    sha256 = "1a16g359kg1gyai1dg6jif72nbrvzpwn04kz6ywkx69rikzsd5wf";
   };
-
-  app = lhs: rhs: { inherit lhs rhs; role = "application"; };
-
-  var = id: bound: { inherit bound id; role = "variable"; type = "unknown"; };
-
-  con = symbol: { inherit symbol; role = "constant"; type = "unknown"; };
-
-  example = [
-    { relation = "~=";
-      rhs      = var 1 false;
-      lhs      = app (app (con "global64")
-                          (app (app (con "global65")
-                                    { role = "lambda";
-                                      arg  = null;
-                                      body = var 0 true; })
-                               (con "Global6c")))
-                     (var 1 false); }
-    { relation = "~=";
-      rhs      = var 0 false;
-      lhs      = app (app (con "global64")
-                          (app (app (con "global65")
-                                    { role = "lambda";
-                                      arg  = null;
-                                      body = var 0 true; })
-                               (con "Global6c")))
-                     (var 0 false); }
-  ];
-
-  test = runCommand "render-eqs-test"
-    {
-      buildInputs = [ untested ];
-      example     = builtins.toJSON example;
-    }
-    ''
-      set -e
-      OUTPUT=$(echo "$example" | renderEqs)
-      for PAT in '~=' 'global64' 'global65' 'Global6c' 'v0' 'v1' 'λ'
-      do
-        echo "$OUTPUT" | grep "$PAT" > /dev/null ||
-          fail "'$PAT' didn't match\n$OUTPUT"
-      done
-      mkdir "$out"
-    '';
 };
-withDeps [ test ] untested
+callPackage "${src}" {}
